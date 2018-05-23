0fe2256df0a2ee28b4e7390a4eb309d7d407e027
Johann Og <kronolynx@gmail.com>
2018-05-23 17:35:04 +0200

    Refactor seed
    Split seed into mnemonic and seed

--- app/lib/crypto/seed.ts
+++ app/lib/crypto/seed.ts
@@ -1,32 +1,14 @@
+import {Mnemonic} from "./mnemonic"
 const bip39 = require("bip39")
 
-/**
- * Seed
- */
-export class Seed {
-  /** mnemonics */
-  private mnemonics: String
-  /** seed */
-  private seed: Buffer
-  public constructor(mnemonics?: String) {
-    if (mnemonics) {
-      if (!bip39.validateMnemonic(mnemonics)) {
-        throw new Error("Invalid mnemonic")
-      }
-      this.mnemonics = mnemonics
-    } else {
-      this.mnemonics = bip39.generateMnemonic()
-    }
-    // compute only once during instantiation
-    this.seed = bip39.mnemonicToSeed(this.mnemonics)
-  }
-  /** get the mnemonic  */
-  public getMnemonics(): String {
-    return this.mnemonics
-  }
+export type Seed = Buffer
 
-  /** get the seed of the mnemonic */
-  public getSeedBuffer(): Buffer {
-    return this.seed
-  }
+export namespace Seed {
+   export const deriveSeedFromMnemonics = (mnemonics: string) => {
+     if (Mnemonic.validate(mnemonics)) {
+       throw new Error("Invalid mnemonic")
+     }
+
+     return bip39.mnemonicToSeed(mnemonics)
+   }
 }
