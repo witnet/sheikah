a135d4326140267cd83bdacef85afefb61859326
Johann Og <kronolynx@gmail.com>
2018-05-23 16:29:31 +0200

    Refactor key path
    Replace class with type and namespace

--- app/lib/keyPath.ts
+++ app/lib/keyPath.ts
@@ -1,4 +1,7 @@
 import * as _ from "lodash"
+
+export type KeyPath = Array<number>
+
 /**
  * Path levels
  * https://github.com/aesedepece/WIPs/blob/wip-adansdpc-hdwallets/wip-adansdpc-hdwallets.md#path-levels
@@ -7,23 +10,12 @@ import * as _ from "lodash"
  *
  * Apostrophe in the path indicates that hardened derivation is used
  */
-export class KeyPath {
-  /** Hardened key index */
-  static readonly hardenedKeyIndex = 0x80000000
+export namespace KeyPath {
+  const hardenedKeyIndex = 0x80000000
 
-  constructor(private path: Array<number> = []) {
-  }
-  /** Last child number */
-  lastChildNumber(): number {
-    return _.last(this.path) || 0
-  }
   /** Path derivation */
-  derive(number: number) {
-    return new KeyPath(_.concat(this.path, number))
-  }
-  /** Key path to string */
-  toString() {
-    return _.map(this.path, KeyPath.childNumberToString).reduce((a, b) => `${a}/${b}`, "m")
+  export const derive = (path: KeyPath, number: number): KeyPath => {
+    return _.concat(path, number)
   }
 
   /**
@@ -33,17 +25,22 @@ export class KeyPath {
    *  hardened version of the key index (e.g. m/44'/0'/0'/0)
    * @returns {KeyPath}
    */
-  static fromString(path: string): KeyPath {
+  export const fromString = (path: string): KeyPath => {
     const toNumber = (value: string): number => {
       const number = _.endsWith(value, "'") ?
-          KeyPath.hardened(_.parseInt(value.slice(0, -1))) : _.parseInt(value)
+          hardened(_.parseInt(value.slice(0, -1))) : _.parseInt(value)
       if (isNaN(number)) {throw Error("Invalid number")}
 
       return number
     }
     const path1 = _.trimStart(_.trimStart(path, "m"), "/")
 
-    return path1.length === 0 ? new KeyPath() : new KeyPath(_.map(path1.split("/"), toNumber))
+    return path1.length === 0 ? [] : _.map(path1.split("/"), toNumber)
+  }
+
+  /** Key path to string */
+  export const toString = (path: KeyPath) => {
+    return _.map(path, childNumberToString).reduce((a, b) => `${a}/${b}`, "m")
   }
 
   /**
@@ -51,9 +48,12 @@ export class KeyPath {
    * @param {number} childNumber
    * @returns {string}
    */
-  static childNumberToString(childNumber: number) {
-    return this
-      .isHardened(childNumber) ? `${childNumber - KeyPath.hardenedKeyIndex}'` : `${childNumber}`
+  export const childNumberToString = (childNumber: number) => {
+    return isHardened(childNumber) ? `${childNumber - hardenedKeyIndex}'` : `${childNumber}`
+  }
+
+  export const lastChildNumber = (path: KeyPath): number => {
+    return _.last(path) || 0
   }
 
   /**
@@ -61,12 +61,12 @@ export class KeyPath {
    * @param {number} index
    * @returns {number}
    */
-  static hardened(index: number) {
-    return KeyPath.hardenedKeyIndex + index
+  export const hardened = (index: number) => {
+    return hardenedKeyIndex + index
   }
+
   /** check if index belongs to hardened key path */
-  /* tslint:disable-next-line: prefer-function-over-method */
-  private static isHardened(index: number) {
-    return index >= KeyPath.hardenedKeyIndex
+  export const isHardened = (index: number) => {
+    return index >= hardenedKeyIndex
   }
 }
