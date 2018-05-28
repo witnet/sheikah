e250799f88d77aa9fbd238b8e661163d3bf988b9
Johann Og <kronolynx@gmail.com>
2018-05-28 15:19:12 +0200

    build(crypto): use secp256k1 types

--- app/lib/crypto/publicKey.ts
+++ app/lib/crypto/publicKey.ts
@@ -1,7 +1,7 @@
-const secp256k1 = require("secp256k1")
+import {publicKeyCreate} from "secp256k1"
 
 export type PublicKey = Buffer
 
 export const create = (privateKey: Buffer, compressed = true): PublicKey => {
-  return secp256k1.publicKeyCreate(privateKey, compressed)
+  return publicKeyCreate(privateKey, compressed)
 }
