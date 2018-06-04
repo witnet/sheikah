03d629caf3b422cd0662d367872b757e93c24465
Johann Og <kronolynx@gmail.com>
2018-06-04 15:24:11 +0200

    feat(crypto): derive child key for public key

--- app/lib/crypto/key/publicKey.ts
+++ app/lib/crypto/key/publicKey.ts
@@ -1,21 +1,55 @@
-import {publicKeyCreate} from "secp256k1"
-import {PrivateKey} from "./privateKey"
+import {publicKeyCreate, publicKeyTweakAdd} from "secp256k1"
+import {PrivateKey, SECP256K1_N} from "./privateKey"
 import {ChainCode, ExtendedKey, getExtendedKey as extendedKey, Key} from "./key"
+import {sha512hmac} from "../hash"
+import {integerAsBuffer} from "../../utils/conversions"
+import BigNum = require("bignum")
+import * as assert from "assert"
 
 export interface PublicKey extends Key {
   type: "public"
 }
 
+/**
+ * Create an Extended public key from a private extended key
+ * @param {ExtendedKey<PrivateKey>} privateExtendedKey
+ * @param {boolean} compressed
+ * @returns {ExtendedKey<PublicKey>}
+ */
 export const create =
-  (privateKey: ExtendedKey<PrivateKey>, compressed = true): ExtendedKey<PublicKey> => {
-    const bytes: Buffer = publicKeyCreate(privateKey.key.bytes, compressed)
+  (privateExtendedKey: ExtendedKey<PrivateKey>, compressed = true): ExtendedKey<PublicKey> => {
+    const bytes: Buffer = publicKeyCreate(privateExtendedKey.key.bytes, compressed)
 
     return {
       key: {type: "public", bytes},
-      chainCode: privateKey.chainCode
+      chainCode: privateExtendedKey.chainCode
     }
   }
 
 export const getExtendedKey = (bytes: Buffer, chainCode: ChainCode): ExtendedKey<PublicKey> => {
   return extendedKey(bytes, chainCode, "public")
+}
+
+export const deriveChildKey = (parentKey: ExtendedKey<PublicKey>,
+                               childIndex: number): ExtendedKey<PublicKey> => {
+  const childIndexBuffer = integerAsBuffer(childIndex)
+  const data = Buffer.concat([parentKey.key.bytes, childIndexBuffer],
+    parentKey.key.bytes.length + childIndexBuffer.length)
+  const I: Buffer = sha512hmac(parentKey.chainCode, data)
+  const IL = I.slice(0, 32)
+  const IR = I.slice(32, 64)
+  const p = BigNum.fromBuffer(IL)
+  // Private point should be less than the secp256k1 order
+  assert(p.cmp(SECP256K1_N) <= 0, "can't generate child public key")
+
+  // Ki = point(parse256(IL)) + Kpar
+  //    = G*IL + Kpar
+  const Ki = publicKeyTweakAdd(parentKey.key.bytes, IL)
+
+  if (Ki === null) {
+    // In case Ki is the point at infinity, proceed with the next value for i
+    return deriveChildKey(parentKey, childIndex + 1)
+  }
+
+  return getExtendedKey(Ki, IR)
 }
--- test/app/lib/crypto/key.spec.ts
+++ test/app/lib/crypto/key.spec.ts
@@ -14,21 +14,21 @@ describe("Key derivation", () => {
     PrivateKey.getExtendedKey(masterSecret, chainCode)
 
   it("should encode private key", () => {
-
     const derivedKey = PrivateKey.derive(masterKey, 0)
     expect(derivedKey.key.bytes.toString("hex"))
       .toEqual("4e2cdcf2f14e802810e878cf9e6411fc4e712edf19a06bcfcc5d5572e489a3b7")
     expect(derivedKey.chainCode.toString("hex"))
       .toEqual("d323f1be5af39a2d2f08f5e8f664633849653dbe329802e9847cfc85f8d7b52a")
   })
-  it("should encode hardened private key", () => {
 
+  it("should encode hardened private key", () => {
     const derivedKeyHardened = PrivateKey.derive(masterKey, hardened(0))
     expect(derivedKeyHardened.key.bytes.toString("hex"))
       .toEqual("edb2e14f9ee77d26dd93b4ecede8d16ed408ce149b6cd80b0715a2d911a0afea")
     expect(derivedKeyHardened.chainCode.toString("hex"))
       .toEqual("47fdacbd0f1097043b78c63c20c34ef4ed9a111d980047ad16282c7ae6236141")
   })
+
   it("should generate public key", () => {
     const public_key = PublicKey.create(masterKey)
     expect(public_key.key.bytes.toString("hex"))
@@ -36,4 +36,13 @@ describe("Key derivation", () => {
     expect(public_key.chainCode.toString("hex"))
       .toEqual("873dff81c02f525623fd1fe5167eac3a55a049de3d314bb42ee227ffed37d508")
   })
-})
+
+  it("should derive public child key ", () => {
+    const public_key = PublicKey.create(masterKey)
+    const der = PublicKey.deriveChildKey(public_key, 0)
+
+    expect(der.key.bytes.toString("hex"))
+      .toEqual("027c4b09ffb985c298afe7e5813266cbfcb7780b480ac294b0b43dc21f2be3d13c")
+    expect(der.chainCode.toString("hex"))
+      .toEqual("d323f1be5af39a2d2f08f5e8f664633849653dbe329802e9847cfc85f8d7b52a")
+  })
