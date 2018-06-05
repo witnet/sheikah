c0c418581c0b8c754cf8156c792c06ece6dcef1c
Johann Og <kronolynx@gmail.com>
2018-06-05 17:34:21 +0200

    feat(crypto): implement derive for public key

--- app/lib/crypto/key/publicKey.ts
+++ app/lib/crypto/key/publicKey.ts
@@ -5,6 +5,7 @@ import {sha512hmac} from "../hash"
 import {integerAsBuffer} from "../../utils/conversions"
 import BigNum = require("bignum")
 import * as assert from "assert"
+import * as KeyPath from "../keyPath"
 
 export interface PublicKey extends Key {
   type: "public"
@@ -30,8 +31,31 @@ export const getExtendedKey = (bytes: Buffer, chainCode: ChainCode): ExtendedKey
   return extendedKey(bytes, chainCode, "public")
 }
 
-export const deriveChildKey = (parentKey: ExtendedKey<PublicKey>,
-                               childIndex: number): ExtendedKey<PublicKey> => {
+/**
+ * Derive a descendant extended public key from a parent extended public key, using either a
+ * keypath or child index.
+ *
+ * @param {ExtendedKey<PublicKey>} parentKey
+ * @param {KeyPath | string | number} keyPath  or index
+ * @returns {any}
+ */
+export const derive =
+  (parentKey: ExtendedKey<PublicKey>, keyPath: KeyPath.KeyPath | string | number) => {
+    let extendedKey: ExtendedKey<PublicKey>
+    if (typeof keyPath !== "number") {
+      const path =
+        typeof keyPath === "string" ? KeyPath.fromString(keyPath) : keyPath
+      extendedKey = path.reduce(deriveChildKey, parentKey)
+    } else {
+      // with child index
+      extendedKey = deriveChildKey(parentKey, keyPath)
+    }
+
+    return extendedKey
+  }
+
+const deriveChildKey = (parentKey: ExtendedKey<PublicKey>,
+                        childIndex: number): ExtendedKey<PublicKey> => {
   const childIndexBuffer = integerAsBuffer(childIndex)
   const data = Buffer.concat([parentKey.key.bytes, childIndexBuffer],
     parentKey.key.bytes.length + childIndexBuffer.length)
