e228fd5931cf6043b5d347d2a5d0fb26cae58812
Johann Og <kronolynx@gmail.com>
2018-06-08 13:45:56 +0200

    test(crypto): p2pkh address encoding

---
+++ test/app/lib/crypto/address.spec.ts
@@ -0,0 +1,12 @@
+import {getMasterKey} from "./helpers"
+import * as PublicKey from "../../../../app/lib/crypto/key/publicKey"
+import {encode} from "../../../../app/lib/crypto/address/p2pkh"
+
+describe("p2pkh", () => {
+  const masterKey = getMasterKey("000102030405060708090a0b0c0d0e0f")
+
+  it("should encode address", () => {
+    const publicKey = PublicKey.create(masterKey)
+    expect(encode(publicKey.key, "wit")).toEqual("wit1qrjjds4kvrr95kyydrs2klu8k85")
+  })
+})
