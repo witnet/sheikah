d2722403faa76711a0db7da02edd7964e22bc21f
Johann Og <kronolynx@gmail.com>
2018-06-08 12:53:19 +0200

    feat(crypto): add sha256 function

--- app/lib/crypto/hash.ts
+++ app/lib/crypto/hash.ts
@@ -18,3 +18,22 @@ const hmacFunction = (hashFunctionName: string, key: Buffer) => {
 export const sha512hmac = (key: Buffer, data: Buffer) => {
   return hmacFunction("sha512", key)(data)
 }
+
+/**
+ * Create sha function for the indicated hash function
+ * @param {string} hashFunctionName name of a hash function
+ * @param {Buffer} bytes
+ * @returns {Buffer} sha function
+ */
+const shaFunction = (hashFunctionName: string, bytes: Buffer) => {
+  return crypto.createHash(hashFunctionName).update(bytes).digest()
+}
+
+/**
+ * sha256
+ * @param {Buffer} bytes
+ * @returns {Buffer}
+ */
+export const sha256 = (bytes: Buffer): Buffer => {
+  return shaFunction("sha256", bytes)
+}
