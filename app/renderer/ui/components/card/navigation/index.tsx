6825f6d1e7f67c2f3c647e2450dfd0ffe131956d
Tommytrg <tomastrg22@gmail.com>
2018-06-15 11:55:24 +0200

    feat: feat(ui-compo) add card with previous/next navigation UI component
    re #153

---
+++ app/renderer/ui/components/card/navigation/index.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+
+import { Card } from "antd"
+
+import { ButtonNavigation } from "../../button/index"
+
+const styles = require("./style.scss")
+
+export interface Iprops {
+  className?: string
+  nextStep?: any
+  previousStep?: any
+  title: string
+}
+
+/**
+ * Card with navigation UI component
+ *
+ * @export
+ * @class Cardnavigation
+ * @extends {React.Component<Iprops>}
+ */
+
+export default class Cardnavigation extends React.Component<Iprops> {
+  // tslint:disable-next-line: completed-docs
+  public render() {
+
+    return (
+      <div className={this.props.className}>
+        <Card title={this.props.title}>
+          <div className={styles.content}>
+            {this.props.children}
+          </div>
+          <div className={styles.navigation}>
+            <ButtonNavigation text="Back" onClick={this.props.previousStep} />
+            <ButtonNavigation text="Next" onClick={this.props.nextStep} />
+          </div>
+        </Card>
+      </div>
+    )
+  }
+}
