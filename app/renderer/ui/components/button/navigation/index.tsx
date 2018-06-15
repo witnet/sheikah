ea20c9b042640089c0f5873f6e68f484190acf23
Tommytrg <tomastrg22@gmail.com>
2018-06-15 11:49:25 +0200

    feat: feat(ui-compo) add navigation button UI component
    re #149

---
+++ app/renderer/ui/components/button/navigation/index.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+
+import { Button } from "antd"
+
+const styles = require("./style.scss")
+
+export interface Iprops {
+  className?: string
+  onClick?: any
+  text: any,
+
+}
+
+/**
+ * Navigation button UI component
+ *
+ * @export
+ * @class ButtonNavigation
+ * @extends {React.Component<Iprops>}
+ */
+
+export default class ButtonNavigation extends React.Component<Iprops> {
+  // tslint:disable-next-line: completed-docs
+  public render() {
+    const navigationClasses = `${styles.button} ${this.props.className}`
+
+    return (
+      <Button className={navigationClasses} onClick={this.props.onClick}>
+        {this.props.text}
+      </Button>
+    )
+  }
+}
