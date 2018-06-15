fcedadd173f23ca040c8c2703c55d9dee4b69f2b
Tommytrg <tomastrg22@gmail.com>
2018-06-15 11:46:54 +0200

    feat: feat(ui-compo) add default button UI component
    re #147

---
+++ app/renderer/ui/components/button/default/index.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+
+import { Button } from "antd"
+
+export interface Iprops {
+  className?: string
+  onClick?: any
+  text: string,
+
+}
+
+/**
+ * Default button UI component
+ *
+ * @export
+ * @class ButtonDefault
+ * @extends {React.Component<Iprops>}
+ */
+
+export default class ButtonDefault extends React.Component<Iprops> {
+  // tslint:disable-next-line: completed-docs
+  public render() {
+    return (
+      <Button className={this.props.className} onClick={this.props.onClick}>
+        {this.props.text}
+      </Button>
+    )
+  }
+}
