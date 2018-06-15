ccbdee7cf6befb5d6ce60ddd15f410e7455736a1
Tommytrg <tomastrg22@gmail.com>
2018-06-15 11:52:38 +0200

    feat: feat(ui-compo) add button interface file

---
+++ app/renderer/ui/components/button/index.tsx
@@ -0,0 +1,11 @@
+import ButtonDefault from "./default/index"
+import ButtonLink from "./link/index"
+import ButtonNavigation from "./navigation/index"
+import ButtonOption from "./option/index"
+
+export {
+  ButtonDefault,
+  ButtonLink,
+  ButtonNavigation,
+  ButtonOption
+}
