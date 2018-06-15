5c6088722e5f69c5048a075733e97211af1b1f88
Tommytrg <tomastrg22@gmail.com>
2018-06-15 11:53:44 +0200

    feat: feat(ui-compo) add default card UI component
    re #152

---
+++ app/renderer/ui/components/card/default/index.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+
+import { Card } from "antd"
+
+const styles = require("./style.scss")
+
+export interface Iprops {
+  className?: string
+  navigation?: boolean
+  nextStep?: any
+  previousStep?: any
+  title: string
+}
+
+/**
+ * Default card UI component
+ *
+ * @export
+ * @class CardDefault
+ * @extends {React.Component<Iprops>}
+ */
+export default class CardDefault extends React.Component<Iprops> {
+  // tslint:disable-next-line: completed-docs
+  public render() {
+    return (
+      <div className={this.props.className}>
+        <Card title={this.props.title}>
+          <div className={styles.content}>
+            {this.props.children}
+          </div>
+        </Card>
+      </div>
+    )
+  }
+}
