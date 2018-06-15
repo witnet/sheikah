898be0915a3e78a2977b73fab50ff7b9e6aedd3f
Tommytrg <tomastrg22@gmail.com>
2018-06-15 12:06:39 +0200

    feat: feat(ui-compo) add signup page and their step components

---
+++ app/renderer/ui/containers/SignupPage.tsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { RouteComponentProps } from "react-router"
+
+import Signup from "../components/signup/index"
+
+/**
+ * Sigup page UI component
+ *
+ * @export
+ * @class SignupPage
+ * @extends {React.Component<RouteComponentProps<any>, void>}
+ */
+
+export class SignupPage extends React.Component<RouteComponentProps<any>, void> {
+  /** render */
+  // tslint:disable-next-line:prefer-function-over-method
+  public render() {
+    return (
+      <Signup />
+    )
+  }
+}
+
+export default (
+  SignupPage as any as React.StatelessComponent<RouteComponentProps<any>>
+)
