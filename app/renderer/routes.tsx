c2857bdf497d736cf9401ae84f754de465f31702
Tommytrg <tomastrg22@gmail.com>
2018-06-15 12:30:48 +0200

    feat: feat(ui-compo) replace root route by signup page

--- app/renderer/routes.tsx
+++ app/renderer/routes.tsx
@@ -1,14 +1,15 @@
 import * as React from "react"
 import { Switch, Route } from "react-router"
 import App from "./containers/App"
-import HomePage from "./containers/HomePage"
+import SignupPage from "./ui/containers/SignupPage"
 import CounterPage from "./containers/CounterPage"
+// import HomePage from "./containers/HomePage"
 
 export default () => (
   <App>
     <Switch>
       <Route path="/counter" component={CounterPage} />
-      <Route path="/" component={HomePage} />
+      <Route path="/" component={SignupPage} />
     </Switch>
   </App>
 )
