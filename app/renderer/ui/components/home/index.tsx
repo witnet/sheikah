e9fc462323ee0f4be1ef64168a2171fb8b0101dc
Tommytrg <tomastrg22@gmail.com>
2018-06-22 14:03:16 +0200

    wip

--- app/renderer/ui/components/home/index.tsx
+++ app/renderer/ui/components/home/index.tsx
@@ -7,7 +7,7 @@ import BlockExplorer from "./views/BlockExplorer/index";
 import Community from "./views/Community/index";
 import SmartContracts from "./views/SmartContracts/index";
 import Wallet from "./views/Wallet/index";
-
+import TopBar from "../topBar/index";
 
 const styles = require("./style.scss")
 
@@ -28,6 +28,8 @@ export default class Home extends React.Component<Iprops> {
       [key: string]: string
     }
 
+    type topBarDataType = { [key: string] : { text: string, link: string}[] }
+
     const components: Icomponents = {
       Attestations,
       BlockExplorer,
@@ -38,21 +40,42 @@ export default class Home extends React.Component<Iprops> {
 
     const paths: Ipaths = {
       Wallet: '/home/wallet',
+      WalletTransactions: "/home/wallet/transactions",
+      WalletReceive: "/home/wallet/receive",
+      WalletSend: "/home/wallet/send",
+      WalletCoins: "/home/wallet/coins",
+
       SmartContracts: "/home/smart-contracts",
       Attestations: "/home/attestations",
       BlockExplorer: "/home/block-explorer",
       Community: "/home/community"
     }
 
+    const topBarData: topBarDataType = {
+      Wallet: [
+        { text: "Transactions", link: "/home/wallet/transactions" },
+        { text: "Receive", link: "/home/wallet/receive" },
+        { text: "Send", link: "/home/wallet/send" },
+        { text: "Coins", link: "/home/wallet/coins" },
+      ]
+    }
+
     const path = this.props.location.pathname
-    const componentName: string = Object.keys(paths).reduce((acc, key) => path === paths[key] ? key : acc, "Wallet")
-    const Content = components[componentName]
+
+    const componentName: string = Object.keys(paths)
+      .reduce((acc, key) => path === paths[key] ? key : acc, "Wallet")
+
+    const topBarContent = topBarData[componentName] || topBarData["Wallet"]
+
+    const ContentComponent = components[componentName]
+    console.log(path)
+    console.log(componentName)
     return (
       <div className={styles.layout}>
       <Siderbar className={styles.sidebar}/>
         <div className={styles.main}>
-          <div className={styles["top-bar"]}>Top Bar</div>
-          <Content />
+          <TopBar className={styles["top-bar"]} links={topBarContent} />
+          <ContentComponent />
         </div>
       </div>
     )
