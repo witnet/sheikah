a283bda7caacf8366e1315ff8cf7576525cb29f0
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-06-25 11:52:07 +0200

    feat(ui): get ConnectedRouter working

--- app/renderer/ui/components/home/index.tsx
+++ app/renderer/ui/components/home/index.tsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 
-import Siderbar from "../sidebar/index";
+import Siderbar from "../sidebar/index"
 
-import Attestations from "./views/Attestations/index";
-import BlockExplorer from "./views/BlockExplorer/index";
-import Community from "./views/Community/index";
-import SmartContracts from "./views/SmartContracts/index";
-import Wallet from "./views/Wallet/index";
-import TopBar from "../topBar/index";
+import Attestations from "./views/Attestations/index"
+import BlockExplorer from "./views/BlockExplorer/index"
+import Community from "./views/Community/index"
+import SmartContracts from "./views/SmartContracts/index"
+import Wallet from "./views/Wallet/index"
+import TopBar from "../topBar/index"
 
 const styles = require("./style.scss")
 
@@ -18,17 +18,16 @@ export interface Iprops {
 export default class Home extends React.Component<Iprops> {
 
   /** render */
-  // tslint:disable-next-line:prefer-function-over-method
   public render() {
     interface Icomponents {
-      [key: string]: any;
+      [key: string]: any
     }
 
     interface Ipaths {
       [key: string]: string
     }
 
-    type topBarDataType = { [key: string] : { text: string, link: string}[] }
+    type topBarDataType = { [key: string]: Array<{ text: string, link: string}> }
 
     const components: Icomponents = {
       Attestations,
@@ -39,7 +38,7 @@ export default class Home extends React.Component<Iprops> {
     }
 
     const paths: Ipaths = {
-      Wallet: '/home/wallet',
+      Wallet: "/home/wallet",
       WalletTransactions: "/home/wallet/transactions",
       WalletReceive: "/home/wallet/receive",
       WalletSend: "/home/wallet/send",
@@ -65,11 +64,12 @@ export default class Home extends React.Component<Iprops> {
     const componentName: string = Object.keys(paths)
       .reduce((acc, key) => path === paths[key] ? key : acc, "Wallet")
 
-    const topBarContent = topBarData[componentName] || topBarData["Wallet"]
+    const topBarContent = topBarData[componentName] || topBarData.Wallet
 
     const ContentComponent = components[componentName]
     console.log(path)
     console.log(componentName)
+
     return (
       <div className={styles.layout}>
       <Siderbar className={styles.sidebar}/>
