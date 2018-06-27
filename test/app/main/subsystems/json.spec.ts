56dc76615f05eb642c2a3f7d41cc331794dbb14c
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-06-27 13:08:42 +0200

    test(json): add tests for jsonSerializer subsystem
    re #188

---
+++ test/app/main/subsystems/json.spec.ts
@@ -0,0 +1,25 @@
+import { jsonFixtures } from "./json.fixtures"
+import { jsonSerializer } from "app/common/serializers"
+
+describe("JsonSerializer", () => {
+
+  jsonFixtures.valid.forEach(({value, serialized}, index) => {
+    it(`should serialize object #${index}`, async () => {
+      const _serialized = await jsonSerializer.serialize(value)
+      expect(_serialized).toEqual(serialized)
+    })
+    it(`should deserialize object #${index}`, async () => {
+      const deserialized = await jsonSerializer.deserialize(serialized)
+      expect(deserialized).toEqual(value)
+    })
+  })
+
+  jsonFixtures.invalid.forEach(({serialized, error}, index) => {
+    it(`should fail to deserialize non-JSON input #${index}`, async () => {
+      jsonSerializer.deserialize(serialized).catch((e) => {
+        expect(e).toEqual(error)
+      })
+    })
+  })
+
+})
