0f8d3819dda60824252356fbf54cbe1ca3027ecd
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-06-27 13:08:42 +0200

    test(json): add tests for jsonSerializer subsystem
    re #188

---
+++ test/app/main/subsystems/json.fixtures.ts
@@ -0,0 +1,37 @@
+const errorFactory = (errorName: string, message: string): Error => {
+  const error = new Error(message)
+  error.name = errorName
+
+  return error
+}
+
+export const jsonFixtures = {
+  valid: [
+    {
+      value: {
+        number: 1337,
+        string: "Hello World",
+        array: [1337, "Hello World", {foo: "bar"}],
+        object: {
+          foo: "bar"
+        }
+      },
+      serialized: `{"number":1337,"string":"Hello World","array":[1337,"Hello World",`
+        + `{"foo":"bar"}],"object":{"foo":"bar"}}`
+    }
+  ],
+  invalid: [
+    {
+      serialized: "",
+      error: errorFactory("SyntaxError", "Unexpected end of JSON input")
+    },
+    {
+      serialized: "{\"",
+      error: errorFactory("SyntaxError", "Unexpected end of JSON input")
+    },
+    {
+      serialized: "{'",
+      error: errorFactory("SyntaxError", "Unexpected token ' in JSON at position 1")
+    }
+  ]
+}
