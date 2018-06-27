a3f0c8b3da5d7b6ccf555f94c14becb3246c63ec
Anler Hernández Peral <anler@anler.me>
2018-06-27 17:42:39 +0200

    fix(yarn start): port fix for appHtml not found to main/index.ts

--- config/paths.js
+++ config/paths.js
@@ -1,6 +1,7 @@
 const path = require("path")
 
-module.exports = {
-  templateAppHtml: path.resolve(__dirname, "../app/renderer/ui/app.html"),
-  filenameAppHtml: path.resolve(__dirname, "../dist/app.html")
-}
+/** @type {string} */
+module.exports.templateAppHtml = path.resolve(__dirname, "../app/renderer/ui/app.html")
+
+/** @type {string} */
+module.exports.filenameAppHtml = path.resolve(__dirname, "../dist/app.html")
