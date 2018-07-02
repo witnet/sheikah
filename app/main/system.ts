d10eeea3faed689da6dfd3393071f88502888771
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-07-02 19:10:26 +0200

    refactor(system): revamp system architecture
    Subsystems can now be either stateless objects or instances of classes, as long as they abide by the
    Lifecycle interface.

--- app/main/system.ts
+++ app/main/system.ts
@@ -1,39 +1,109 @@
-import * as serializers from "app/common/serializers"
-import * as components from "app/main/subsystems"
-import log from "app/common/logging"
 import { Config } from "app/common/config"
+import log from "app/common/logging"
+import { JsonSerializer } from "app/common/serializers"
 import { Lifecycle } from "./lifecycle"
-
-export type Json = { json: serializers.JsonSerializer }
+import { jsonSubSystem } from "./subsystems/json"
+import { JsonPlainLevel, JsonPlainLevelStorage } from "./subsystems/storages/jsonPlainLevel"
 
 /**
- * A type representing all the values returned when the system starts.
+ * Type of the system object returned by system.start()
  */
-export type System = Json
+export type SubSystems = {
+  json: JsonSerializer,
+  appStorage: JsonPlainLevelStorage
+}
+
+/**
+ * Type of the "builders" object defined down below
+ */
+export type Builders = {
+  [K in keyof SubSystems]: [Lifecycle<SubSystems[K], Partial<Config>>, Partial<Config>]
+}
+
+/**
+ * Object containing all the subsystems that we want to start along the appSystem itself.
+ * @type Builders
+ */
+const builders: Builders = {
+  json: [jsonSubSystem, {}],
+  appStorage: [new JsonPlainLevel(), { name: "appStorage" }]
+}
 
 /**
  * Application wide system that starts all its components when started.
  * A system is just stateful object that supports Lifecycle protocol (it can be started and stopped)
- * and returns a map of type `System` that can be used as a global repository of services needed by
- * other parts of the application. Any part of the application can explicitly ask for all the
- * `System` or just a subset of it, e.g.: `Json & Db`.
+ * and returns a map of type `SubSystems` that can be used as a global repository of services needed
+ * by other parts of the application. Any part of the application can explicitly ask for all the
+ * `SubSystems` or just a subset of it, e.g.: `json & appStorage`.
  */
-export const appSystem: Lifecycle<System, Partial<Config>> = {
-  async start(config?: Partial<Config>) {
-    log.info("Starting system...")
-    const [json] = await Promise.all([
-      components.json.start()
-    ])
-    log.info("System started")
+export class System implements Lifecycle<SubSystems, Partial<Config>> {
 
-    return { json }
-  },
+  /**
+   * Flag indicating whether the system has been started or not
+   * @type {boolean}
+   */
+  public started = false
 
-  async stop() {
-    log.info("Stopping system...")
-    await Promise.all([
-      components.json.stop()
-    ])
-    log.info("System stopped")
+  /**
+   * Object containing all the started subsystems.
+   */
+  public subSystems: SubSystems
+
+  /**
+   * System start function.
+   * It starts the system and all its subsystems.
+   * @param config
+   */
+  public async start(config?: Partial<Config>) {
+    if (this.started) {
+      log.error("System already started!")
+    } else {
+      log.info("Starting system...")
+
+      const promises: Array<Promise<SubSystems[keyof SubSystems]>> = Object.entries(builders)
+        .map(async ([name, [cycle, config]]) => {
+          log.info(`\tStarting subsystem "${name}"...`)
+          log.debug(`\t\tConfig options are: ${JSON.stringify(config)}`)
+
+          return cycle.start(config)
+        })
+
+      const values = await Promise.all(promises)
+      this.subSystems = Object.keys(builders)
+        .reduce((acc, name, index) => {
+          return {...acc, [name]: values[index]}
+        }, {} as SubSystems)
+
+      this.started = true
+      log.info("System started")
+    }
+
+    return this.subSystems
   }
+
+  /**
+   * System stopper function.
+   * It stops the system and all its subsystems.
+   */
+  public async stop() {
+    if (this.started) {
+      log.info("Stopping system...")
+
+      const promises: Array<Promise<void>> = Object.entries(builders)
+        .map(async ([name, [cycle]]) => {
+          log.info(`\tStopping subsystem "${name}"...`)
+
+          return cycle.stop()
+        })
+
+      await Promise.all(promises)
+      this.subSystems = {} as SubSystems
+      log.info("System stopped")
+    } else {
+      log.error("System already stopped!")
+    }
+  }
+
 }
+
+export const appSystem = new System()
