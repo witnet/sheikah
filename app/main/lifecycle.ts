06a4d3de0c3fa06d7706ccf5e40bda6430ce7d61
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-07-02 19:02:42 +0200

    refactor(lifecycle): turn Lifecycle into an interface + update naming
    Lifecycle is now an interface so that it can be extended by classes.
    
    Components are now called SubSystems.

--- app/main/lifecycle.ts
+++ app/main/lifecycle.ts
@@ -1,18 +1,18 @@
-export type Lifecycle<T, C> = {
+export interface Lifecycle<T, C> {
   start(config?: Partial<C>): Promise<T>
   stop(): Promise<void>
 }
 
 /**
- * Helper function for creating components. A component is just an object that implements the
- * lifecycle protocol. A pure component is just a component that do not need to do any real
+ * Helper function for creating subsystems. A subsystem is just an object that implements the
+ * lifecycle protocol. A pure subsystem is just a subsystem that do not need to do any real
  * initialization/teardown when starting/stopping.
  *
- * @param {(config: C) => T} getValue A function that given an optional config object
- * returns the value of the component.
- * @returns {Lifecycle<T, C>} The component object.
+ * @param {(config: C) => T} getValue A function that given an optional config object returns the
+ * value of the subsystem.
+ * @returns {Lifecycle<T, C>} The subsystem object.
  */
-export function pureComponent<T, C>(getValue: (config?: C) => T): Lifecycle<T, C> {
+export function pureSubsystem<T, C>(getValue: (config?: C) => T): Lifecycle<T, C> {
   return {
     async start(config?: C) {
       return getValue(config)
