d5e57b13ee9dcbfb36f67aa9cd5542be17de57a6
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-07-03 19:02:11 +0200

    feat(subsystems): add WalletStorageCollectionSubSystem
    fix #197

---
+++ app/main/subsystems/storages/wallets.ts
@@ -0,0 +1,43 @@
+import { Config } from "app/common/config"
+import { Lifecycle } from "app/main/lifecycle"
+import Storage from "./index"
+
+type WalletStorage = Storage<string, Buffer, Buffer, Buffer>
+
+type WalletStorageCollection = {[name: string]: WalletStorage}
+
+/**
+ * This is a very simple subsystem containing a collection of WalletStorage objects that can be
+ * easily accessed by name.
+ */
+export class WalletStorageCollectionSubSystem
+  implements Lifecycle<WalletStorageCollection, Partial<Config>> {
+
+  /**
+   * This is the actual collection where references to the WalletStorage objects are stored.
+   * @type WalletStorageCollection
+   */
+  private items: WalletStorageCollection = {}
+
+  /**
+   * Lifecycle start function.
+   * It simply returns a reference to the internal WalletStorage collection.
+   */
+  public async start(): Promise<WalletStorageCollection> {
+    return this.items
+  }
+
+  /**
+   * Lifecycle stop function.
+   * It closes all referenced WalletStorages and cleans after itself.
+   */
+  public async stop() {
+    const promises = Object.values(this.items)
+      .map((storage: WalletStorage) => storage.close())
+    await Promise.all(promises)
+    this.items = {}
+
+    return
+  }
+
+}
