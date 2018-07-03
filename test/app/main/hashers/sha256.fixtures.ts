a30aa66a4d2062665634e17577ca43cdca279989
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-07-03 11:19:20 +0200

    test(serializers): update sha256 tests to cover string and buffer modes
    After fb0e57104baebb26df8ada92fd4f084d6279961a

--- test/app/main/hashers/sha256.fixtures.ts
+++ test/app/main/hashers/sha256.fixtures.ts
@@ -1,7 +1,4 @@
-import {sha256Hasher} from "../../../../app/main/hashers/sha256"
-
 const sha256fixtures = {
-  hasher: sha256Hasher,
   pairs: [
     ["Hello, World!", "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f"]
   ]
