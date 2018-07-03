d5ad8950758f93a0bc6be2560c0b107f65035be8
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-07-03 11:30:41 +0200

    test(storage): add new storage tests
    Covers jsonPlainInMemory and jsonAesInMemory

---
+++ test/app/main/storage/jsonPlainInMemory.spec.ts
@@ -0,0 +1,42 @@
+import { jsonSerializer } from "app/common/serializers"
+import { PlainCipher } from "app/main/ciphers/plain"
+import { noHasher } from "app/main/hashers/no"
+import InMemoryPersister from "app/main/persisters/inMemory"
+import Storage from "app/main/subsystems/storages"
+
+const keyHasher = noHasher
+const serializer = jsonSerializer
+const cipher = new PlainCipher<Buffer>()
+const backend = new InMemoryPersister()
+
+const storage = new Storage(keyHasher, serializer, cipher, backend)
+
+describe("jsonPlainInMemory", () => {
+  it("should put a string", async () => {
+    await storage.put("foo", "bar")
+  })
+
+  it("should put a number", async () => {
+    await storage.put("fee", 1234)
+  })
+
+  it("should put an object", async () => {
+    await storage.put("faa", { beer: "cold" })
+  })
+
+  it("should get a string", async () => {
+    const res = await storage.get("foo")
+    expect(res).toEqual("bar")
+  })
+
+  it("should get a number", async () => {
+    const res = await storage.get("fee")
+    expect(res).toEqual(1234)
+  })
+
+  it("should get an object", async () => {
+    const res = await storage.get("faa")
+    expect(res).toEqual({ beer: "cold" })
+  })
+
+})
