786fc40d7f7e6382030607739c0f8c1e04fd23bb
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-07-11 17:14:51 +0200

    feat(renderer): make index request wallet info and put it into store
    re #182

--- app/renderer/index.tsx
+++ app/renderer/index.tsx
@@ -1,27 +1,43 @@
+import { asRuntimeType, Contexts } from "app/common/runtimeTypes"
+import { Wallets } from "app/common/runtimeTypes/storage/wallets"
+import { Client } from "app/renderer/api"
 import * as React from "react"
 import { render } from "react-dom"
 import { AppContainer } from "react-hot-loader"
 import Root from "./containers/Root"
 import "./ui/app.global.scss"
 
-const { configureStore, history } = require("./store/configureStore")
-const store = configureStore()
+const client = new Client()
 
-render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById("root")
-)
+new Promise(async () => {
+  // Query and parse wallets from main process
+  const _wallets = await client.request("getWallets")
+  const wallets = asRuntimeType(_wallets, Wallets, Contexts.IPC)
 
-if ((module as any).hot) {
-  (module as any).hot.accept("./containers/Root", () => {
-    const NextRoot = require("./containers/Root").default
-    render(
-      <AppContainer>
-        <NextRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById("root")
-    )
-  })
-}
+  // Configure and initialize Redux store
+  const { configureStore, history } = require("./store/configureStore")
+  const store = configureStore({ wallets })
+
+  // Root component rendering
+  render(
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById("root")
+  )
+
+  // Hot-reload functionality
+  if ((module as any).hot) {
+    (module as any).hot.accept("./containers/Root", () => {
+      const NextRoot = require("./containers/Root").default
+      render(
+        <AppContainer>
+          <NextRoot store={store} history={history} />
+        </AppContainer>,
+        document.getElementById("root")
+      )
+    })
+  }
+}).catch((error) => {
+  console.error("Unhandled renderer process boot up error", error)
+})
