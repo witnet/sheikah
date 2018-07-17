c0de96625b8dcf5fa4ed5a2a85606e9383385711
Mario Cao <mario@stampery.co>
2018-07-17 17:26:46 +0200

    feat(rt-types): add types for unconsolidated wallet and mnenomics
    re #220

--- app/common/runtimeTypes/storage/wallets.ts
+++ app/common/runtimeTypes/storage/wallets.ts
@@ -8,3 +8,20 @@ export type WalletInfo = t.TypeOf<typeof WalletInfo>
 
 export const Wallets = t.array(WalletInfo)
 export type Wallets = t.TypeOf<typeof Wallets>
+
+const Mnemonics = t.type({
+  mnemonics: t.string
+})
+
+export const NewMnemonicsResponse = t.exact(Mnemonics)
+export type NewMnemonicsResponse = t.TypeOf<typeof NewMnemonicsResponse>
+
+// UnconsolidatedWallet represents a transient wallet which is being created
+export const UnconsolidatedWallet = t.intersection([
+  Mnemonics,
+  t.partial({
+    id: t.string,
+    caption: t.string
+  })
+])
+export type UnconsolidatedWallet = t.TypeOf<typeof UnconsolidatedWallet>
