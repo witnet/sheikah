6f16b6012ce28932f089d5fe9650e5c0b910c0ca
mmartinbar <maria@stampery.co>
2018-07-26 16:10:20 +0200

    feat(reducers): add reducers for wallet and forms parts of the store
    Add reducer functions for the actions that can be triggered from the soft login page
    
    re #231

--- app/renderer/reducers/wallets.ts
+++ app/renderer/reducers/wallets.ts
@@ -1,10 +1,8 @@
 import { Wallets } from "app/common/runtimeTypes/storage/wallets"
 import { IAction } from "app/renderer/actions/helpers"
 
-export type WalletsState = Wallets
+const defaultWalletsState: Wallets = []
 
-const defaultWalletsState: WalletsState = []
-
-export const walletsReducer = (state = defaultWalletsState, action: IAction): WalletsState => {
+export const walletsReducer = (state = defaultWalletsState, action: IAction): Wallets => {
   return state
 }
