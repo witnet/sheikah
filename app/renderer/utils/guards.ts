012ca03ae0cbdea204058948040c109ee0adaa50
mmartinbar <maria@stampery.co>
2018-07-26 16:28:05 +0200

    feat(guards): add guard to check the existence of a wallet in the store
    A new guard has been added to test if a wallet exists in the store. Guard functions (and related
    types) have been moved to their own files.
    
    re #231

---
+++ app/renderer/utils/guards.ts
@@ -0,0 +1,31 @@
+import { Store } from "redux"
+import { StoreState } from "app/renderer/store"
+
+/**
+ * All route guards must abide by this type.
+ */
+type RouteGuard = (store: Store<StoreState>) => (props: any) => boolean
+
+/**
+ * This guard checks if there are wallets in the store.
+ * @param store
+ */
+export const ifWallets: RouteGuard = (store) => {
+  const state = store.getState()
+
+  return (props: any) => {
+    return state.wallets.length > 0
+  }
+}
+
+/**
+ * This guard checks if there is a wallet in the store.
+ * @param store
+ */
+export const ifWallet: RouteGuard = (store) => {
+  const state = store.getState()
+
+  return (props: any) => {
+    return (state.wallet !== false)
+  }
+}
