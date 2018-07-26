9bf319bd91b72a4aa8d6649ab4d6fbd353550294
mmartinbar <maria@stampery.co>
2018-07-26 16:22:26 +0200

    feat(utils): add component to conditionally render a compo or redirect
    RedirectedRoute is a component that will render a component or redirect to a path depending on the
    evaluation of a guard function
    
    re #231

---
+++ app/renderer/utils/redirectedRoute.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { Redirect, Route } from "react-router"
+
+/**
+ * A guarded route renders component A or B depending on the result of executing a guard function
+ * with the arguments passed to the route.
+ */
+export class RedirectedRoute extends React.Component<any> {
+
+  /**
+   * Guarded route render function.
+   */
+  public render() {
+    const { aCompo: A, redirect: location, guard, ...props } = this.props
+
+    const renderer = (props: any) => {
+      return guard(props) ? <A {...props} /> : location ? <Redirect to={{ pathname: location }} /> :
+        <div>The guard of a guarded route failed without a redirect</div>
+    }
+
+    return <Route {...props} render={renderer} />
+  }
+}
