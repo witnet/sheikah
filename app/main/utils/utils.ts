05504b7d2802902c0c459c342d2396bd5c82e474
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-07-31 16:44:05 +0200

    feat(handlers): add fanOut() promises helper for handlers

--- app/main/utils/utils.ts
+++ app/main/utils/utils.ts
@@ -32,3 +32,17 @@ export function kvSwap(obj: stringToNumber | numberToString | stringToString): a
 export function inject<T, U, V>(fn: (arg: T, injected: V) => U, injected: V) {
   return (arg: T): U => fn(arg, injected)
 }
+
+/**
+ * Converts a list of homogeneous functions into an asynchronous function that feeds a single input
+ * argument to all the listed functions and returns a list with the matching outputs.
+ * By "homogeneous" we mean that all the functions share the same contract: each of them accepts a
+ * single input argument of type I and return a single value of type O.
+ * TODO: with TS 3.x, we will be able to generalize fanOut() to accept non-homogeneous functions.
+ * @param fns
+ */
+export function fanOut<I, O>(fns: Array<(arg: I) => O>) {
+  return async (arg: I) => Promise.all(
+    fns.map(async (fn) => Promise.resolve(arg).then(fn))
+  )
+}
