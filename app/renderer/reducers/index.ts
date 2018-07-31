8c56560b5aaec536c8ee1dddbe8c23b5d68029e4
mmartinbar <maria@stampery.co>
2018-07-31 11:49:43 +0200

    feat(reducers): remove reducers for form state in redux
    Transitive form state is local to the container, not in redux and so reducers are not needed
    
    re #282

--- app/renderer/reducers/index.ts
+++ app/renderer/reducers/index.ts
@@ -1,14 +1,12 @@
 import { combineReducers, Reducer } from "redux"
 import { walletsReducer } from "app/renderer/reducers/wallets"
 import { walletReducer } from "app/renderer/reducers/wallet"
-import { formsReducer } from "app/renderer/reducers/forms"
 import { StoreState } from "app/renderer/store"
 
 // Combine all the relevant Redux reducers
 const rootReducer = combineReducers({
   wallets: walletsReducer,
   wallet: walletReducer,
-  forms: formsReducer,
 }) as Reducer<StoreState>
 
 export default rootReducer
