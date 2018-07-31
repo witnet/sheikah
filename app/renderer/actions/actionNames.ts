b5fd08727006bc007cdb6a3dacdef68b7a0e8920
mmartinbar <maria@stampery.co>
2018-07-31 11:54:44 +0200

    feat(actions): remove actions for form state in redux
    Transitive form state is local to the container, not in redux and so actions are not needed
    
    re #282

--- app/renderer/actions/actionNames.ts
+++ app/renderer/actions/actionNames.ts
@@ -1,9 +1,2 @@
 // Names of the action types for the wallet reducer
 export const SAVE_WALLET = "SAVE_WALLET"
-
-// Names of the action types for the forms reducer
-export const UPDATE_SOFT_LOGIN = "UPDATE_SOFT_LOGIN"
-
-// Names of the action types for the soft-login reducer under the forms reducer
-export const SET_FIELD = "SL_SET_FIELD"
-export const DELETE_FIELD = "SL_DELETE_FIELD"
--- app/renderer/actions/actionTypes.ts
+++ app/renderer/actions/actionTypes.ts
@@ -1,13 +1,5 @@
-import { SoftLoginForm } from "app/renderer/store"
 import { IActionWithPayload } from "app/renderer/actions/helpers"
 import { Wallet } from "app/common/runtimeTypes/storage/wallets"
-
-// Types for the actions that arrive at the soft login reducer
-export type SoftLoginKey = keyof SoftLoginForm
-export type SoftLoginAction = IActionWithPayload<Partial<SoftLoginForm> | SoftLoginKey>
-
-// Types for the actions that arrive at the forms reducer
-export type FormsAction = IActionWithPayload<SoftLoginAction>
 
 // Types for the actions that arrive at the wallet reducer
 export type SaveWalletAction = IActionWithPayload<Wallet>
