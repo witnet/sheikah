1b4055f180f1b1fc4d8a5aaf83efac3073eb9c3d
mmartinbar <maria@stampery.co>
2018-08-01 13:19:17 +0200

    feat(actions): modify return types of unlockWallet action creator
    UnlockWallet action creator returns the wallet to the container in the case of a successful unlock.
    It returns an error message in the case of an unsuccessful unlock. The error message will be
    specific in the case of a handler error and generic in the case of any IPC related error.
    
    re #282

--- app/renderer/actions/loginForm.ts
+++ app/renderer/actions/loginForm.ts
@@ -6,28 +6,38 @@ import { Services } from "app/renderer/services"
 import {
   SaveWalletAction,
 } from "./actionTypes"
-import { push } from "connected-react-router"
+import { GetWalletResponse } from "app/common/runtimeTypes/ipc/wallets"
+
+// Function to assert a never value
+const assertNever = (x: never) => undefined
 
 // Action creator to unlock a wallet
 export const unlockWallet = (id: string, password: string) => {
   return async (dispatch: any, getState: any, services: Services) => {
     return new Promise((resolve, reject) => {
       // Try to retrieve wallet from renderer API & dispatch wallet or error
-      // TODO dispatch(push("/")) needs to be replaced by dispatch(push(MAIN_PATHS.MAIN)
       api.getWallet(services.apiClient, id, password)
-        .then((wallet: Wallet) => {
-          dispatch(saveWallet(wallet))
-          dispatch(push("/"))
-          resolve("")
+        .then((walletResponse: GetWalletResponse) => {
+          // Check wallet response type
+          switch (walletResponse.kind) {
+            case "success":
+              resolve(walletResponse.wallet)
+              break
+            case "error":
+              reject(walletResponse.error)
+              break
+            default:
+              assertNever(walletResponse)
+          }
         })
-        .catch((err: Error) => {
-          reject(err.message)
+        .catch((error) => {
+          reject("UNKNOWN ERROR")
         })
     })
   }
 }
 
-// Internal action creator to save wallet
-const saveWallet = (wallet: Wallet): SaveWalletAction => {
+// Action creator to save wallet
+export const saveWallet = (wallet: Wallet): SaveWalletAction => {
   return actionCreator<Wallet>(Actions.SAVE_WALLET)(wallet)
 }
