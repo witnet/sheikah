72f0f58d3e10e6a2e20a0c75a37dd77d143d59a8
mmartinbar <maria@stampery.co>
2018-08-01 13:15:14 +0200

    feat(renderer): add error handling in getWallet renderer api function
    Both handler errors and generic IPC errors are handled in renderer api function. Generic IPC errors
    are abstracted to the caller into one common error. Handler errors are not abstracted and the caller
    will be able to treat them separately.
    
    re #282

--- app/renderer/api/getWallet.ts
+++ app/renderer/api/getWallet.ts
@@ -1,15 +1,40 @@
+import * as t from "io-ts"
+
 import { Contexts, asRuntimeType } from "app/common/runtimeTypes"
 import { ApiClient } from "app/renderer/api"
-import { Wallet } from "app/common/runtimeTypes/storage/wallets"
+import {
+  GetWalletResponse,
+  getWalletErrors,
+  GetWalletErrors,
+  buildGetWalletError
+} from "app/common/runtimeTypes/ipc/wallets"
 
 /**
  * Function to request the unlock of a wallet through the API Client
  * @param client
  */
-export async function getWallet(client: ApiClient, id: string, password: string): Promise<Wallet> {
-  // Request the unlock of a wallet
-  const wallet = await client.request("getWallet", { id, password })
+export async function getWallet(client: ApiClient, id: string,
+  password: string): Promise<GetWalletResponse> {
+  return client.request("getWallet", { id, password })
+    .then(parseResponse)
+    .catch(buildError)
+}
 
-  // Cast return value to runtime type
-  return asRuntimeType(wallet, Wallet, Contexts.IPC)
+/**
+ * Function to check that the received response is a valid response
+ * @param response
+ */
+function parseResponse(response: any) {
+  try {
+    return asRuntimeType(response, GetWalletResponse, Contexts.IPC)
+  } catch (error) {
+    throw getWalletErrors.GENERIC_IPC_ERROR
+  }
+}
+
+/**
+ * Function to build a generic IPC error that abstracts from IPC errors
+ */
+function buildError() {
+  return buildGetWalletError(t.literal<GetWalletErrors>(getWalletErrors.GENERIC_IPC_ERROR.value))
 }
