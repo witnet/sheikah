11c2a9cde80a80971065e9d406570adfe014c795
Johann Og <kronolynx@gmail.com>
2018-08-07 10:21:00 +0200

    feat(handlers): make newWallet handler accept xprv seeds
    closes #260

--- app/main/api/handlers/importSeed.ts
+++ app/main/api/handlers/importSeed.ts
@@ -11,9 +11,12 @@ import { AppStateManager } from "app/main/appState"
 import * as mnemonic from "app/main/crypto/mnemonic"
 
 import { AppStateS } from "app/main/system"
-import { fanOut, inject, pick } from "app/main/utils/utils"
+import { fanOut, inject } from "app/main/utils/utils"
 import * as crypto from "crypto"
 import { LiteralType } from "io-ts"
+import { Seed } from "app/common/runtimeTypes/storage/wallets"
+import * as Slip32 from "slip32"
+import { fromMnemonics } from "app/main/crypto/seed"
 
 /**
  * Handler function for `importSeed` method.
@@ -28,20 +31,95 @@ export default async function importSeed
 
   // First of all, parse method parameters
   return parseParams(params)
-    // Pick only the value of the mnemonics key from the handler parameters
-    .then(pick("mnemonics"))
-    // In parallel: validate mnemonic, check mnemonic against unconsolidated wallet and generate id
-    .then(fanOut([validateMnemonicsString, inject(matchMnemonics, appStateManager), generateId]))
+    // Get the seed from the params
+    .then(inject(getSeed, appStateManager))
+    .then(buildUnconsolidatedWallet)
+    // .then(fanOut([validateMnemonicsString, inject(matchMnemonics, appStateManager), generateId]))
     // Update or insert unconsolidated wallet into app state
     .then(inject(upsertUnconsolidated, appStateManager))
     // The handler logic ends here. What follows is response building and encoding.
-
     // If everything was OK, return success
     .then(buildSuccessResponse)
     // If anything failed, return error
     .catch(buildErrorResponse)
     // Encode response
     .then(encodeResponse)
+}
+
+/**
+ * Get seed from params
+ * @param params
+ * @param appStateManager
+ */
+async function getSeed(params: ImportSeedParams, appStateManager: AppStateManager): Promise<Seed> {
+  let seed: Promise<Seed>
+  switch (params.kind) {
+    case "mnemonics":
+      seed = processMnemonics(params.mnemonics, appStateManager)
+      break
+    case "xprv":
+      seed = processXprv(params.xprv)
+      break
+    default:
+      // compiler instruction to ensure exhaustiveness
+      const _never: never = params
+      throw importSeedErrors.INVALID_METHOD_PARAMS
+  }
+
+  return seed
+}
+
+/**
+ * Validate mnemonics and get seed
+ * @param mnemonics
+ * @param appStateManager
+ */
+async function processMnemonics(mnemonics: string, appStateManager: AppStateManager):
+  Promise<Seed> {
+  return Promise.resolve(mnemonics)
+    // In parallel: validate mnemonic, check mnemonic against unconsolidated wallet and generate id
+    .then(fanOut([validateMnemonicsString, inject(matchMnemonics, appStateManager)]))
+    .then(seedFromMnemonics)
+}
+
+/**
+ * Generates an ID and builds and returns an UnconsolidatedWallet
+ * @param seed
+ */
+async function buildUnconsolidatedWallet(seed: Seed) {
+  return {
+    id: generateId(seed),
+    seed
+  }
+}
+
+/**
+ * Get seed from xprv
+ * @param xprv
+ */
+async function processXprv(xprv: string): Promise<Seed> {
+  try {
+    const key = Slip32.importKeyFromSlip32(xprv)
+
+    return {
+      chainCode: Buffer.from(key.extendedKey.chainCode),
+      masterSecret: Buffer.from(key.extendedKey.key.bytes)
+    }
+  } catch (error) {
+    throw importSeedErrors.INVALID_XPRV
+  }
+}
+
+/**
+ * Get seed from mnemonics
+ * @param param0
+ */
+function seedFromMnemonics([mnemonics]: Array<string>): Seed {
+  try {
+    return fromMnemonics(mnemonics)
+  } catch (error) {
+    throw importSeedErrors.INVALID_MNEMONICS
+  }
 }
 
 /**
@@ -57,10 +135,10 @@ async function parseParams(params: any): Promise<ImportSeedParams> {
  * Generate an id string given a mnemonics string.
  * @param mnemonics
  */
-function generateId(mnemonics: string): string {
+function generateId(seed: Seed): string {
   try {
     return crypto.pbkdf2Sync(
-      mnemonics,
+      Buffer.concat([seed.chainCode, seed.masterSecret]),
       config.mnemonicsIdGeneration.salt,
       config.mnemonicsIdGeneration.hashIterations,
       config.mnemonicsIdGeneration.keyByteLength,
@@ -101,9 +179,9 @@ function matchMnemonics(mnemonics: string, appStateManager: AppStateManager): st
  * @param unconsolidated
  */
 function upsertUnconsolidated
-  ([_, mnemonics, id]: Array<string>, appStateManager: AppStateManager): string {
+  ({ id, seed }: { id: string, seed: Seed }, appStateManager: AppStateManager): string {
   try {
-    appStateManager.update({ unconsolidatedWallet: { id, mnemonics } })
+    appStateManager.update({ unconsolidatedWallet: { id, seed } })
 
     return id
   } catch {
