efae38b859dfb59532437db3ba66af03a27e460e
Tommytrg <tomastrg22@gmail.com>
2018-08-08 11:20:54 +0200

    feat(ui-compo): update wallet form seed backup component

---
+++ app/renderer/ui/components/walletForm/steps/walletSeedBackup/index.tsx
@@ -0,0 +1,55 @@
+import * as React from "react"
+
+import { CardNavigation } from "app/renderer/ui/components/card"
+import { AlertDefault } from "app/renderer/ui/components/alert"
+
+const styles = require("./style.scss")
+
+export interface Iprops {
+  className?: string
+  mnemonics: string,
+  nextStep: () => void
+  previousStep: () => void
+}
+
+/**
+ * Step wallet seedd backup UI component
+ *
+ * @export
+ * @class WalletSeedBackup
+ * @extends {React.Component<Iprops>}
+ */
+
+export default class WalletSeedBackup extends React.Component<Iprops> {
+  /** render */
+  // tslint:disable-next-line:prefer-function-over-method
+  public render() {
+    return (
+      <>
+        <CardNavigation
+          className={this.props.className}
+          title="Wallet seed creation"
+          previousStep={this.props.previousStep}
+          nextStep={this.props.nextStep}
+        >
+          <p>Your wallet seed secret code is:</p>
+          <div className={styles.seed}>
+            <p>{this.props.mnemonics}</p>
+          </div>
+          <p>Please save this 12 words on paper (order is important). This seed will allow you to
+            recover your wallet in case of computer failure.</p>
+        </CardNavigation>
+        <AlertDefault className={styles["alert-grid"]}>
+          <div className={styles.alert}>
+            <p>Remember!</p>
+            <ul>
+              <li>Never disclose your seed.</li>
+              <li>Never type on a website</li>
+              <li>Do not store electronically</li>
+            </ul>
+          </div>
+        </AlertDefault>
+      </>
+    )
+  }
+}
