093e2b0fc69e581e7996035f7bb993a6d255ad8a
mmartinbar <maria@stampery.co>
2018-08-08 18:44:52 +0200

    feat(utils): add PropsRoute util component
    PropsRoute is an auxiliary component that renders a Route component and merges RouteComponentProps
    and additional props in the render function. In turn, that render function renders a different
    component which receives those merged props.
    
    re #311

---
+++ app/renderer/utils/propsRoute.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+import { Route, RouteComponentProps } from "react-router"
+
+import { RoutesProps } from "app/renderer/routes"
+
+/**
+ * A PropsRoute component renders a Route in a path given by props.
+ *
+ * The render function of that Route will render a component given by props to PropsRoute
+ * and the props of that component will be a merge between RouteComponentProps and
+ * additional props (RoutesProps)
+ */
+export class PropsRoute extends React.Component<any> {
+  /**
+   * Function to render a component combining RouteComponentProps with RoutesProps
+   * as props
+   */
+  private withOwnProps = (A: typeof React.Component, routesProps: RoutesProps) =>
+    (props: RouteComponentProps<any>) => <A {...routesProps} {...props} />
+
+  /**
+   * PropsRoute render function.
+   */
+  public render() {
+    const { path, ownProps, component } = this.props
+
+    return <Route path={path} render={this.withOwnProps(component, ownProps)} />
+  }
+}
