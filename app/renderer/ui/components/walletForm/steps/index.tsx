03dde6f9325a7092861a8df5da309c2fb6b190e0
Tommytrg <tomastrg22@gmail.com>
2018-08-08 11:30:43 +0200

    feat(ui-compo): update wallet form components' api file

---
+++ app/renderer/ui/components/walletForm/steps/index.tsx
@@ -0,0 +1,15 @@
+import AddressesGeneration from "./adressesGeneration/index"
+import WalletEncryptionPassword from "./walletEncryptionPassword/index"
+import WalletSeedBackup from "./walletSeedBackup/index"
+import WalletSeedConfirmation from "./walletSeedConfirmation/index"
+import WalletSeedTypeSelection from "./walletSeedTypeSelection/index"
+import Welcome from "./welcome/index"
+
+export {
+  AddressesGeneration,
+  WalletEncryptionPassword,
+  WalletSeedBackup,
+  WalletSeedConfirmation,
+  WalletSeedTypeSelection,
+  Welcome
+}
