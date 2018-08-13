90525add990bce4bf7e02e96eb3f1a4fb699ebb4
Tommytrg <tomastrg22@gmail.com>
2018-08-13 13:02:53 +0200

    fix(routes): fix wallet seed confirmation route

--- app/renderer/constants/urls.ts
+++ app/renderer/constants/urls.ts
@@ -6,7 +6,7 @@ export const NEW_WALLET = `${FORMS}/wallet`
 export const WELCOME = `${NEW_WALLET}/welcome`
 export const WALLET_SEED_TYPE_SELECTION = `${NEW_WALLET}/wallet-seed-type-selection`
 export const WALLET_SEED_BACKUP = `${NEW_WALLET}/wallet-seed-backup`
-export const WALLET_SEED_CONFIRMATION = `${NEW_WALLET}wallet-seed-confirmation`
+export const WALLET_SEED_CONFIRMATION = `${NEW_WALLET}/wallet-seed-confirmation`
 export const WALLET_ENCRYPTION_PASSWORD = `${NEW_WALLET}/wallet-encryption-password`
 
 // LoginForm URLS
