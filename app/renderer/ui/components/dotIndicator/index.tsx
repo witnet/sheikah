7abc6a8571481644706d772f8853c83998327900
Tommytrg <tomastrg22@gmail.com>
2018-08-16 19:39:45 +0200

    feat(ui-dot-indicator): colored dot indicators UI polish
    fix #345

---
+++ app/renderer/ui/components/dotIndicator/index.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+
+const styles = require("./style.scss")
+
+export interface Props {
+  mood: "positive" | "neutral" | "warning" | "negative"
+}
+
+/**
+ * Dot indicator UI component
+ *
+ * @export
+ * @class DotIndicator
+ * @extends {React.Component<Props>}
+ */
+
+export default class DotIndicator extends React.Component<Props> {
+  // tslint:disable-next-line: completed-docs
+  public render() {
+    const dotStyle = `fa fa-circle ${styles[this.props.mood]}`
+
+    return (
+      <i className={dotStyle} />
+    )
+  }
+}
+
+export { DotIndicator }
