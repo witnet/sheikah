3599abbf9c383b98f6e1fa1878aee46be85745e3
mmartinbar <maria@stampery.co>
2018-08-17 17:30:52 +0200

    feat(ui-compo): add ActionButton ui component for the blue buttons
    re #347

---
+++ app/renderer/ui/components/button/action/index.tsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+
+import { Button } from "antd"
+
+const styles = require("./style.scss")
+
+export interface Iprops {
+  className?: string
+  onClick?: any
+  disabled?: boolean
+}
+
+/**
+ * Action button UI component
+ *
+ * @export
+ * @class ActionButton
+ * @extends {React.Component<Iprops>}
+ */
+
+export default class ActionButton extends React.Component<Iprops> {
+  // tslint:disable-next-line: completed-docs
+  public render() {
+    return (
+      <Button
+        className={`${styles.button} ${this.props.className}`}
+        onClick={this.props.onClick}
+        disabled={this.props.disabled}
+      >
+        {this.props.children}
+      </Button>
+    )
+  }
+}
