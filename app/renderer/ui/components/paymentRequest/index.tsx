7f3322181470016365062b381521e596b803b08e
mmartinbar <maria@stampery.co>
2018-08-29 20:12:42 +0200

    feat(ui-compo): extend payment request component with improved UI
    re #395

--- app/renderer/ui/components/paymentRequest/index.tsx
+++ app/renderer/ui/components/paymentRequest/index.tsx
@@ -1,63 +1,148 @@
 import * as React from "react"
 import { Dropdown } from "app/renderer/ui/components/dropdown"
+import { ComputedPaymentRequest } from "app/renderer/prefilledPaymentRequests"
 
 const styles = require("./style.scss")
 
-type status = "expired" | "still into force" | "never funded"
-
-export interface Iprops {
-  creationDate: string
-  expirationDate?: string
-  status: status
-  from: string
-  amount: string
-  actions: Array<({ text: any; onClick: any })>
-}
+export type OwnProps = ComputedPaymentRequest
 
 /**
  * PaymentRequest UI component
  *
  * @export
  * @class PaymentRequest
- * @extends {React.Component<Iprops>}
+ * @extends {React.Component<OwnProps>}
  */
-
-export default class PaymentRequest extends React.Component<Iprops> {
+export default class PaymentRequest extends React.Component<OwnProps> {
   // tslint:disable-next-line: completed-docs
   public render() {
-    const createStatus = (status: status, date: string | undefined) => (
-      {
-        expired: (
-          <p className={styles["expiration-date"]}>
-            <span className={styles.red}>Still into force</span>, expiring on {date}
-          </p>
-        ),
-        "still into force": (
-          <p className={styles["expiration-date"]}>
-            <span className={styles.green}>Expired</span> on {date}
-          </p>
-        ),
-        "never funded": (
-          <p className={styles["expiration-date"]}>
-            <span className={styles.red}>Never funded</span>
-          </p>
-        )
-      }[status]
-    )
-
     return (
       <div className={styles["payment-request"]}>
-        <div className={styles["creation-date"]}>
-          {this.props.creationDate}
+        <div className={styles.date}>
+          {this.props.metadata && this.props.metadata.creationDate}
         </div>
         <div className={styles.info}>
-          <p>From {this.props.from} ({this.props.amount}WIT)</p>
-          {createStatus(this.props.status, this.props.expirationDate)}
+          <div className={styles.label}>
+            {this.buildLabel()}
+          </div>
+          <div className={styles.status}>
+            {this.buildStatus()}
+          </div>
         </div>
-        <div className={styles.actions}>
-          <Dropdown dataSource={this.props.actions} />
+        <div className={styles.address}>
+          <pre>
+            {this.props.address}
+          </pre>
         </div>
+        <Dropdown
+          className={styles.actions}
+          dataSource={this.props.actions}
+        />
       </div>
+    )
+  }
+
+  /**
+   * Method to get the wit span
+   */
+  private getWitSpan = () => {
+    return (<span className={styles.wit}> WIT</span>)
+  }
+
+  /**
+   * Method to build the label in a payment request
+   */
+  private buildLabel = () => {
+    const fromSpan = this.props.metadata && this.props.metadata.label
+      ? <span>From {this.props.metadata.label} </span>
+      : <></>
+
+    const witSpan = this.props.metadata && this.props.metadata.label
+      ? this.props.metadata.requestedAmount
+        ? (
+          <>
+            <span>({this.props.metadata.requestedAmount}</span>
+            {this.getWitSpan()}
+            <span>)</span>
+          </>
+        )
+        : (
+          <></>
+        )
+      : this.props.funds !== 0
+        ? (
+          <>
+            <span>{this.props.funds}</span>
+            {this.getWitSpan()}
+          </>
+        )
+        : (
+          <></>
+        )
+
+    return (
+      <>
+        {fromSpan}
+        {witSpan}
+      </>
+    )
+  }
+
+  /**
+   * Method to build the status in a payment request
+   */
+  private buildStatus = () => {
+    // Funded status
+    const fundedStatus = {
+      pending: this.props.expiredStatus === "expired"
+        ? (
+          <></>
+        )
+        : (
+          <span className={styles.yellow}>Pending</span>
+        ),
+      "partially funded": (
+        <>
+          <span className={styles.yellow}>Partially funded </span>
+          <span>({this.props.funds}</span>
+          {this.getWitSpan()}
+          <span>)</span>
+        </>
+      ),
+      funded: (
+        <span className={styles.green}>Funded</span>
+      ),
+      overfunded: (
+        <>
+          <span className={styles.green}>Overfunded </span>
+          <span>({this.props.funds}</span>
+          {this.getWitSpan()}
+          <span>)</span>
+        </>
+      )
+    }
+
+    // Expiration status
+    const expiredStatus = {
+      expired: (
+        <>
+          <span className={styles.red}>Expired </span>
+          <span>on {this.props.metadata && this.props.metadata.expirationDate}</span>
+        </>
+      ),
+      "not expired": (
+        <span>, expiring on {this.props.metadata && this.props.metadata.expirationDate}</span>
+      ),
+      unknown: (
+        <></>
+      )
+    }
+
+    return (
+      <>
+        {fundedStatus[this.props.fundedStatus]}
+        {expiredStatus[this.props.expiredStatus]}
+      </>
     )
   }
 }
