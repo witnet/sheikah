e16385242644ba8824ff0fe3b86530562ddffd8d
María <maria@stampery.co>
2018-09-04 14:26:57 +0200

    feat(marketplace): include svg in the marketplace section
    re #462

--- app/renderer/ui/components/main/sections/marketplace/index.tsx
+++ app/renderer/ui/components/main/sections/marketplace/index.tsx
@@ -3,9 +3,8 @@ import * as React from "react"
 import { SectionInfo, SectionProps } from "app/renderer/ui/components/main/sections"
 
 import * as urls from "app/renderer/constants/urls"
-import { CardMarketplaceProduct } from "app/renderer/ui/components/card"
-import { marketplaceProducts } from "app/renderer/ui/components/main/sections/marketplace/MockData"
 
+const svgImage = require("svg/marketplace.svg")
 const styles = require("./style.scss")
 
 /**
@@ -18,25 +17,10 @@ class Marketplace extends React.Component<SectionProps> {
 
   // tslint:disable-next-line:prefer-function-over-method completed-docs
   public render() {
-    const createMarketPlaceProduct = () => marketplaceProducts
-      .map((product) => <CardMarketplaceProduct key={product.title} {...product} />)
-
     return (
-      <>
-        <div className={styles.layout}>
-          <div className={styles.header}>
-            <p className={styles.title}>
-              <span className={styles.sheikah}>Sheikah</span> Marketplace
-            </p>
-            <p className={styles.subtitle}>
-              hundred of smart contracts, ready to be deployed
-            </p>
-          </div>
-          <div className={styles.featured}>
-            {createMarketPlaceProduct()}
-          </div>
-        </div>
-      </>
+      <div className={styles.layout}>
+        <img src={svgImage} />
+      </div>
     )
   }
 }
