f18153095923c07900dbe9638ea978b8f3e447cc
María <maria@stampery.co>
2018-09-04 09:34:05 +0200

    feat(section): add routing to tabs in Data Requests section
    re #448

--- app/renderer/ui/components/main/sections/dataRequests/index.tsx
+++ app/renderer/ui/components/main/sections/dataRequests/index.tsx
@@ -1,7 +1,16 @@
 import * as React from "react"
 
-import { SectionInfo, SectionProps } from "app/renderer/ui/components/main/sections"
+import { Switch } from "react-router"
+
+import { SectionInfo, SectionProps, TabInfo } from "app/renderer/ui/components/main/sections"
 import * as urls from "app/renderer/constants/urls"
+import { TopBarLinkProps } from "app/renderer/ui/components/commonTypes"
+import TopBar from "app/renderer/ui/components/topBar"
+import { PropsRoute } from "app/renderer/utils/propsRoute"
+import { TabHistory, TabEditor } from "app/renderer/ui/components/main/sections/dataRequests/tabs"
+
+const styles = require("./style.scss")
+const mainStyles = require("app/renderer/ui/components/main/style.scss")
 
 /**
  * Data requests UI component
@@ -14,10 +23,42 @@ import * as urls from "app/renderer/constants/urls"
 class DataRequests extends React.Component<SectionProps> {
   // tslint:disable-next-line: completed-docs
   public render() {
+    const tabs: Array<TabInfo> = [TabHistory, TabEditor]
+    const topBarlinkProps: Array<TopBarLinkProps> = tabs.map(tab => (
+      {
+        caption: tab.caption,
+        key: tab.key,
+        path: `${tab.path}`
+      }
+    ))
+
     return (
-      <div className={this.props.className}>
-        Data requests
-      </div>
+      <>
+        <TopBar
+          className={mainStyles["top-bar"]}
+          pathName={this.props.pathName}
+          linksProps={topBarlinkProps}
+        />
+        <div className={styles.layout}>
+          <Switch>
+            <PropsRoute
+              path={urls.DATA_REQUESTS_HISTORY_TAB}
+              ownProps={this.props}
+              component={TabHistory.component}
+            />
+            <PropsRoute
+              path={urls.DATA_REQUESTS_EDITOR_TAB}
+              ownProps={this.props}
+              component={TabEditor.component}
+            />
+            <PropsRoute
+              path="/"
+              ownProps={this.props}
+              component={TabHistory.component}
+            />
+          </Switch>
+        </div>
+      </>
     )
   }
 }
@@ -26,7 +67,7 @@ const DataRequestsSection: SectionInfo = {
   key: "datarequests",
   caption: "Data Requests",
   sectionPath: urls.DATA_REQUESTS_SECTION,
-  path: urls.DATA_REQUESTS_SECTION,
+  path: urls.DATA_REQUESTS_HISTORY_TAB,
   component: DataRequests,
   icon: "eye"
 }
