9fb4711ba302b5c410c55ec15ee9827b413e35a3
María <maria@stampery.co>
2018-09-04 10:16:41 +0200

    feat(coins-tab): remove coins tab
    re #467

--- app/renderer/ui/components/main/sections/wallet/tabs/index.tsx
+++ app/renderer/ui/components/main/sections/wallet/tabs/index.tsx
@@ -1,10 +1,8 @@
-import TabCoins from "./coins"
 import TabReceive from "./receive"
 import TabSend from "./send"
 import TabTransactions from "./transactions"
 
 export {
-  TabCoins,
   TabReceive,
   TabSend,
   TabTransactions
