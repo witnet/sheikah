4769918bbd660a07ce4ec198f5aa741bcb06b962
mmartinbar <maria@stampery.co>
2018-09-05 11:52:52 +0200

    fix(community): fix links in the community section
    fix #508

--- app/renderer/ui/components/main/sections/community/index.tsx
+++ app/renderer/ui/components/main/sections/community/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { SectionInfo, SectionProps } from "app/renderer/ui/components/main/sections"
 import * as urls from "app/renderer/constants/urls"
+import { shell } from "electron"
 
 const emailLogo = require("svg/social/email.svg")
 const githubLogo = require("svg/social/github.svg")
@@ -12,6 +13,15 @@ const telegramLogo = require("svg/social/telegram.svg")
 const twitterLogo = require("svg/social/twitter.svg")
 
 const styles = require("./styles.scss")
+
+/**
+ * Method to open a link in the default external app (browser, mail client...)
+ * @param url
+ */
+const openInExternalApp = (url: string) => (e: React.MouseEvent<HTMLAnchorElement>) => {
+  shell.openExternal(url)
+}
+
 /**
  * Community UI component
  *
@@ -19,7 +29,6 @@ const styles = require("./styles.scss")
  * @class Community
  * @extends {React.Component<SectionProps>}
  */
-
 export class Community extends React.Component<SectionProps> {
   // tslint:disable-next-line: completed-docs prefer-function-over-method
   public render() {
@@ -29,7 +38,10 @@ export class Community extends React.Component<SectionProps> {
         <ul className={styles.channels}>
           <li className={styles["list-item"]}>
             <img className={styles.back} src={twitterLogo} />
-            <a href="//twitter.com/witnet_io" target="_blank">
+            <a
+              onClick={openInExternalApp("https://twitter.com/witnet_io")}
+              target="_blank"
+            >
               Witnet Foundation Twitter account
             </a>
             <p>
@@ -39,29 +51,47 @@ export class Community extends React.Component<SectionProps> {
           </li>
           <li className={styles["list-item"]}>
             <img className={styles.back} src={mediumLogo} />
-            <a href="//medium.com/witnet" target="_blank">
+            <a
+              onClick={openInExternalApp("https://medium.com/witnet")}
+              target="_blank"
+            >
               Witnet Foundation official blog on Medium
             </a>
             <p>
               Follow Witnet Foundation on Medium to read different articles stating our vision and
               future roadmap.
             </p>
-            </li>
+          </li>
           <li className={styles["list-item"]}>
             <img className={styles.back} src={telegramLogo} />
-            <a href="//t.me/witnetio" target="_blank"> Witnet Community on Telegram</a>
+            <a
+              onClick={openInExternalApp("https://t.me/witnetio")}
+              target="_blank"
+            >
+              Witnet Community on Telegram
+            </a>
             <p>Join other members of the Witnet Community at the official Telegram group.</p>
           </li>
           <li className={styles["list-item"]}>
             <img className={styles.back} src={redditLogo} />
-            <a href="//reddit.com/r/witnet" target="_blank">Witnet Community subreddit</a>
+            <a
+              onClick={openInExternalApp("https://reddit.com/r/witnet")}
+              target="_blank"
+            >
+              Witnet Community subreddit
+            </a>
             <p>The space for linking articles, informal discussion, kitten pictures and occasional
               memes.
             </p>
           </li>
           <li className={styles["list-item"]}>
             <img className={styles.back} src={githubLogo} />
-            <a href="//github.com/witnet" target="_blank">Witnet GitHub repository</a>
+            <a
+              onClick={openInExternalApp("https://github.com/witnet")}
+              target="_blank"
+            >
+              Witnet GitHub repository
+            </a>
             <p>
               GitHub is where the Witnet protocol development takes place. Are you a programmer?
               Your contributions are more than welcome!
@@ -69,25 +99,31 @@ export class Community extends React.Component<SectionProps> {
           </li>
           <li className={styles["list-item"]}>
             <img className={styles.back} src={gitterLogo} />
-              <a href="//gitter.im/witnet/community" target="_blank">
-                Witnet Development Community at Gitter
-              </a>
-              <p>
-                Join the official Witnet community Gitter room to discuss with the rest of the
-                community and get direct support from the developers.
-              </p>
-            </li>
-            <li className={styles["list-item"]}>
-              <img className={styles.back} src={emailLogo} />
-              <a href="mailto:info@witnet.foundation?subject=Witnet project" target="_blank">
-                Witnet Foundation contact email
-              </a>
-              <p>
-                You can write us directly for any inquiry not appropriate for any of the other
-                channels.
-              </p>
-            </li>
-          </ul>
+            <a
+              onClick={openInExternalApp("https://gitter.im/witnet/community")}
+              target="_blank"
+            >
+              Witnet Development Community at Gitter
+            </a>
+            <p>
+              Join the official Witnet community Gitter room to discuss with the rest of the
+              community and get direct support from the developers.
+            </p>
+          </li>
+          <li className={styles["list-item"]}>
+            <img className={styles.back} src={emailLogo} />
+            <a
+              onClick={openInExternalApp("mailto:info@witnet.foundation?subject=Witnet project")}
+              target="_blank"
+            >
+              Witnet Foundation contact email
+            </a>
+            <p>
+              You can write us directly for any inquiry not appropriate for any of the other
+              channels.
+            </p>
+          </li>
+        </ul>
       </div>
     )
   }
