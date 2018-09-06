f99aa03e1e9c4c139cd625dab7550b58aa94ed2f
mmartinbar <maria@stampery.co>
2018-09-06 15:46:29 +0200

    feat(reducer): add reducer for the wallets parts in the redux store
    re #522

--- app/renderer/reducers/wallets.ts
+++ app/renderer/reducers/wallets.ts
@@ -1,8 +1,28 @@
+import * as Actions from "app/renderer/actions/actionNames"
 import { WalletInfos, CURRENT_WALLETS_VERSION } from "app/common/runtimeTypes/storage/wallets"
-import { Action } from "redux"
+import { SaveWalletInfosAction, SaveWalletInfoAction } from "app/renderer/actions/actionTypes"
+import * as _ from "lodash"
 
 const defaultWalletsState: WalletInfos = { _v: CURRENT_WALLETS_VERSION, infos: [] }
 
-export const walletsReducer = (state = defaultWalletsState, action: Action): WalletInfos => {
-  return state
+type walletsReducerActions = SaveWalletInfosAction | SaveWalletInfoAction
+
+export const walletsReducer = (
+  state = defaultWalletsState,
+  action: walletsReducerActions
+): WalletInfos => {
+  switch (action.type) {
+    case Actions.SAVE_WALLET_INFOS:
+      return action.payload ? action.payload : state
+    case Actions.SAVE_WALLET_INFO:
+      const newState = _.cloneDeep(state)
+
+      if (newState && action.payload) {
+        newState.infos.push(action.payload)
+      }
+
+      return newState
+    default:
+      return state
+  }
 }
