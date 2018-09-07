witnet/sheikah

commit 571d4a225095f905fa9de7399f74651e28abe675

style(ui): make tabs have scrollable content and fixed menu

app/renderer/ui/components/main/sections/wallet/style.scss
@@ -1,8 +1,10 @@
 :local {
   .layout {
     display: grid;
+    grid-area: ct;
     grid-gap: 3em;
     grid-template-columns: 3fr 1fr;
+    overflow: auto;
     padding: 3em;
   }
 }

app/renderer/ui/components/main/style.scss
@@ -25,18 +25,11 @@
     grid-template-areas:
       "tb"
       "ct";
-    grid-template-rows: repeat(2, 60px 1fr);
-    max-height: 100vh;
-    overflow: scroll;
+    grid-template-rows: 60px calc(100vh - 60px);
   }
 
   .top-bar {
     background: $top_bar-background;
     grid-area: tb;
-  }
-
-  .content {
-    background: $background-color;
-    grid-area: ct;
   }
 }

commit 332326168b200c672b0d8513f43d977fdc7fa2fa

fix(issue): fix sections not scrollable
the bug was introduced in `571d4a225095f905fa9de7399f74651e28abe675`
and fixed by restoring the `overflow: scroll` property of `.main`
class. additionally made the navigation bars for data-requests and
smart-contracts sections fixed.

app/renderer/ui/components/main/sections/dataRequests/style.scss
@@ -1,15 +1,19 @@
 :local {
   .layout {
     display: grid;
+    grid-area: ct;
     grid-gap: 2em;
     grid-template-columns: 3fr 1fr;
+    overflow: auto;
     padding: 0;
   }
 
   .paddedLayout {
     display: grid;
+    grid-area: ct;
     grid-gap: 2em;
     grid-template-columns: 3fr 1fr;
+    overflow: auto;
     padding: 3em;
   }
 }

app/renderer/ui/components/main/sections/dataRequests/tabs/editor/index.tsx
@@ -12,9 +12,7 @@ const svgRetrieval = require("svg/dataRequestsEditorRetrieval.svg")
 const svgAttestation = require("svg/dataRequestsEditorAttestation.svg")
 const svgDelivery = require("svg/dataRequestsEditorDelivery.svg")
 
-const layoutStyles = require("app/renderer/ui/components/main/sections/dataRequests/style.scss")
 const styles = require("./style.scss")
-const tabStyles = require("app/renderer/ui/components/main/sections/dataRequests/tabs/style.scss")
 
 /**
  * Props that contain configuration
@@ -34,83 +32,83 @@ class TabEditor extends TabComponent<any & Props> {
   // tslint:disable-next-line:prefer-function-over-method completed-docs
   public render() {
     return (
-      <div className={layoutStyles.layout}>
-        <div className={`${styles.bar}`}>
-          <img src={svgBar} />
-        </div>
-        <div className={`${tabStyles.left} ${styles.left}`}>
+      <div className={styles.layout}>
+        <img className={styles.bar} src={svgBar} />
+        <div className={styles.left}>
           <Wrapper
+            className={styles.logic}
+            empty={true}
             title="RETRIEVAL LOGIC"
           >
             <img src={svgRetrieval} />
           </Wrapper>
-          <br />
           <Wrapper
+            className={styles.logic}
+            empty={true}
             title="ATTESTATION LOGIC"
           >
             <img src={svgAttestation} />
           </Wrapper>
-          <br />
           <Wrapper
+            className={styles.logic}
+            empty={true}
             title="DELIVERY LOGIC"
           >
             <img src={svgDelivery} />
           </Wrapper>
         </div>
-        <div className={`${tabStyles.right} ${styles.right}`}>
-          <div className={styles.right}>
-            <label className={styles.label}>FILE NAME</label>
-            <InputUnderlined
-              className={styles.input}
-              type="text"
-              value="ethereum-weather-reporter.rad"
-            />
+        <div className={styles.right}>
+          <label className={styles.label}>FILE NAME</label>
+          <InputUnderlined
+            className={styles.input}
+            type="text"
+            value="ethereum-weather-reporter.rad"
+          />
 
-            <label className={styles.label}>TITLE</label>
-            <InputUnderlined
-              className={styles.input}
-              type="text"
-              value="Report weather to Ethereum contract"
-            />
+          <label className={styles.label}>TITLE</label>
+          <InputUnderlined
+            className={styles.input}
+            type="text"
+            value="Report weather to Ethereum contract"
+          />
 
-            <label className={styles.label}>DESCRIPTION</label>
-            <textarea className={`${styles.textarea} ${styles.input}`}>
-              Ask two different APIs for the weather in a certain location at a future
-              point in time, then send result to Ethereum contract.
-            </textarea>
+          <label className={styles.label}>DESCRIPTION</label>
+          <textarea className={`${styles.textarea} ${styles.input}`}>
+            Ask two different APIs for the weather in a certain location at a future
+            point in time, then send result to Ethereum contract.
+          </textarea>
 
-            <p className={styles.title}>PROTOCOL LEVEL PARAMETERS</p>
-            <CardDefault contentStyle={styles.cardContent}>
-              <table>
-                <tbody>
-                  <tr>
-                    <td className={styles.param}><p>datetime</p></td>
-                    <td><p>exec_time</p></td>
-                  </tr>
-                  <tr>
-                    <td className={styles.param}><p>int</p></td>
-                    <td><p>replication_factor</p></td>
-                  </tr>
-                </tbody>
-              </table>
-            </CardDefault>
+          <p className={styles.title}>PROTOCOL LEVEL PARAMETERS</p>
+          <CardDefault contentStyle={styles.cardContent}>
+            <table>
+              <tbody>
+                <tr>
+                  <td className={styles.param}><p>datetime</p></td>
+                  <td><p>exec_time</p></td>
+                </tr>
+                <tr>
+                  <td className={styles.param}><p>int</p></td>
+                  <td><p>replication_factor</p></td>
+                </tr>
+              </tbody>
+            </table>
+          </CardDefault>
 
-            <p className={styles.title}>TEMPLATE SPECIFIC PARAMETERS</p>
-            <CardDefault contentStyle={styles.cardContent}>
-              <table>
-                <tbody>
-                  <tr>
-                    <td className={styles.param}><p>string</p></td>
-                    <td><p>location</p></td>
-                  </tr>
-                  <tr>
-                    <td className={styles.param}><p>string</p></td>
-                    <td><p>eth_address</p></td>
-                  </tr>
-                </tbody>
-              </table>
-            </CardDefault>
-          </div>
+          <p className={styles.title}>TEMPLATE SPECIFIC PARAMETERS</p>
+          <CardDefault contentStyle={styles.cardContent}>
+            <table>
+              <tbody>
+                <tr>
+                  <td className={styles.param}><p>string</p></td>
+                  <td><p>location</p></td>
+                </tr>
+                <tr>
+                  <td className={styles.param}><p>string</p></td>
+                  <td><p>eth_address</p></td>
+                </tr>
+              </tbody>
+            </table>
+          </CardDefault>
         </div>
       </div>
     )

app/renderer/ui/components/main/sections/dataRequests/tabs/editor/style.scss
@@ -10,17 +10,13 @@
     padding: 0;
   }
 
-  .bar {
-    grid-column-end: 3;
-    grid-column-start: 1;
-  }
-
   .editor div {
     padding: 15px !important;
   }
 
   .right {
     font-size: 1em;
+    grid-column: 2;
     padding-right: 3em;
 
     .title {
@@ -46,6 +42,14 @@
 
   .left {
     padding-left: 3em;
+  }
+
+  .layout {
+    display: grid;
+    grid-gap: 2em;
+    grid-template-columns: 1fr 360px;
+    overflow: auto;
+    padding: 0 0 3em 0;
   }
 
   .param {
@@ -83,5 +87,15 @@
 
   .cardContent {
     padding: 20px;
+  }
+
+  .logic {
+    margin-bottom: 20px;
+  }
+
+  .bar {
+    grid-column-end: 3;
+    grid-column-start: 1;
+    width: 100%;
   }
 }

app/renderer/ui/components/main/sections/dataRequests/tabs/history/style.scss
@@ -7,6 +7,7 @@
 
   .right {
     grid-column: 2;
+    min-width: 350px;
 
     .title {
       color: $tooltip_title-color;
@@ -27,5 +28,13 @@
 
   .withPadding div {
     padding: 12px !important;
+  }
+
+  .layout {
+    display: grid;
+    grid-gap: 2em;
+    grid-template-columns: 1fr 360px;
+    overflow: auto;
+    padding: 3em;
   }
 }

app/renderer/ui/components/main/sections/marketplace/style.scss
@@ -2,6 +2,8 @@
   @import "app/renderer/ui/theme";
 
   .layout {
+    height: 100vh;
+    overflow: auto;
     padding: 30px;
   }
 }

app/renderer/ui/components/main/sections/smartContracts/style.scss
@@ -1,14 +1,18 @@
 :local {
   .layout {
     display: grid;
+    grid-area: ct;
     grid-gap: 2em;
-    grid-template-columns: 3fr 1fr;
+    grid-template-columns: 1fr 360px;
+    overflow: auto;
   }
 
   .paddedLayout {
     display: grid;
+    grid-area: ct;
     grid-gap: 2em;
-    grid-template-columns: 3fr 1fr;
+    grid-template-columns: 1fr 360px;
+    overflow: auto;
     padding: 3em;
   }
 }

app/renderer/ui/components/main/sections/smartContracts/tabs/editor/index.tsx
@@ -6,13 +6,10 @@ import * as urls from "app/renderer/constants/urls"
 import Wrapper from "app/renderer/ui/components/wrapper"
 import { Services } from "app/renderer/services"
 
-const svgBar = require("svg/smartContractsEditorBar.svg")
 const svgComposer = require("svg/smartContractsEditorComposer.svg")
 const svgSideContent = require("svg/smartContractsEditorSideContent.svg")
 
-const layoutStyles = require("app/renderer/ui/components/main/sections/smartContracts/style.scss")
 const styles = require("./style.scss")
-const tabStyles = require("app/renderer/ui/components/main/sections/smartContracts/tabs/style.scss")
 
 /**
  * Props that contain configuration
@@ -32,23 +29,30 @@ class TabEditor extends TabComponent<any & Props> {
   // tslint:disable-next-line:prefer-function-over-method completed-docs
   public render() {
     return (
-      <div className={layoutStyles.layout}>
-        <div className={`${styles.bar}`}>
-          <img src={svgBar} />
+      <div className={styles.layout}>
+        <div className={styles.bar}>
+          <p className={styles.first}>
+            <span>Editing </span>
+            <span className={styles.semibold}>'Pay to Alice or Bob, based on API response'</span>
+          </p>
+          <p className={styles.triangle} />
+          <p className={styles.second}>New</p>
+          <p className={styles.third}>Open</p>
         </div>
-        <div className={`${tabStyles.left} ${styles.left}`}>
-          <Wrapper
-            title="CONTRACT LOGIC"
-          >
-            <CardDefault
-              className={styles.editor}
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <Wrapper
+              className={styles.contract}
+              title="CONTRACT LOGIC"
             >
-              <img src={svgComposer} />
-            </CardDefault>
-          </Wrapper>
-        </div>
-        <div className={`${tabStyles.right} ${styles.right}`}>
-          <img src={svgSideContent} />
+              <CardDefault
+                className={styles.editor}
+              >
+                <img src={svgComposer} />
+              </CardDefault>
+            </Wrapper>
+          </div>
+          <img className={styles.right} src={svgSideContent} />
         </div>
       </div>
     )

app/renderer/ui/components/main/sections/smartContracts/tabs/editor/style.scss
@@ -1,18 +1,62 @@
 :local {
+  @import "app/renderer/ui/theme";
+
   .bar {
-    grid-column-end: 3;
-    grid-column-start: 1;
+    align-items: center;
+    background: $smart_contracts_bar-background-light_color;
+    display: flex;
+    height: 60px;
+
+    .first {
+      background: $smart_contracts_bar-background-dark_color;
+      font-style: italic;
+      height: 100%;
+      line-height: 60px;
+      padding: 0 48px;
+    }
+
+    .semibold {
+      font-weight: 600;
+    }
+
+    .triangle {
+      border-bottom: 30px solid transparent;
+      border-left: 30px solid $smart_contracts_bar-background-dark_color;
+      border-top: 30px solid transparent;
+    }
+
+    .second {
+      border-right: 2px solid $smart_contracts_bar-background-dark_color;
+      font-weight: 600;
+      height: 100%;
+      line-height: 60px;
+      padding: 0 48px;
+    }
+
+    .third {
+      font-weight: 600;
+      height: 100%;
+      line-height: 60px;
+      padding: 0 48px;
+    }
   }
 
-  .editor div {
-    padding: 15px !important;
+  .layout {
+    display: grid;
+    overflow: scroll;
   }
 
-  .right {
-    padding-right: 3em;
+  .content {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 3em;
   }
 
   .left {
-    padding-left: 3em;
+    max-width: 1100px;
+    min-width: 570px;
+    padding-right: 1em;
+    width: 100%;
   }
 }

app/renderer/ui/components/main/sections/wallet/style.scss
@@ -3,7 +3,7 @@
     display: grid;
     grid-area: ct;
     grid-gap: 3em;
-    grid-template-columns: 3fr 1fr;
+    grid-template-columns: 1fr 360px;
     overflow: auto;
     padding: 3em;
   }

app/renderer/ui/components/main/style.scss
@@ -26,6 +26,7 @@
       "tb"
       "ct";
     grid-template-rows: 60px calc(100vh - 60px);
+    overflow: scroll;
   }
 
   .top-bar {

app/renderer/ui/theme.scss
@@ -209,6 +209,10 @@ $sidebar_layout-font_size: 22px;
 $sidebar_layout-icon_label-color: $grey-1;
 $sidebar_layout-icon-color: $grey-1;
 
+// SMART_CONTRACTS
+$smart_contracts_bar-background-dark_color: $grey-4;
+$smart_contracts_bar-background-light_color: $grey-3;
+
 // SPINNER
 $spinner-height: 80px;
 $spinner-main-border: 2px dotted $grey-5;