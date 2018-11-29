b6ff86bde51890c33e3beedff40e56dba36eec09
kengork <saracuriel.p@gmail.com>
2018-11-29 11:09:06 +0100

    feat(component): create a checkbox component

---
+++ app/renderer/ui/components/input/checkbox/index.tsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+
+import { Checkbox } from "antd"
+import { CheckboxChangeEvent } from "antd/lib/checkbox"
+
+export interface Iprops {
+    className?: string
+    name?: string
+    onChange?: (e: CheckboxChangeEvent) => void
+
+  }
+
+/**
+ * Checkbox UI component
+ *
+ * @export
+ * @class InputCheck
+ * @extends {React.Component<Iprops>}
+ */
+export default class InputCheck extends React.Component<Iprops> {
+    // tslint:disable-next-line: completed-docs
+    public render() {
+        return(
+            <Checkbox
+                className={this.props.className}
+                onChange={this.props.onChange}
+            />
+        )
+    }
+}
--- app/renderer/ui/components/input/index.tsx
+++ app/renderer/ui/components/input/index.tsx
@@ -2,10 +2,12 @@ import DefaultInput from "./default/index"
 import InputAmount from "./amount/index"
 import InputUnderlined from "./underlined/index"
 import InputBig from "./big/index"
+import InputCheck from "./checkbox/index"
 
 export {
   DefaultInput,
   InputAmount,
   InputBig,
-  InputUnderlined
+  InputUnderlined,
+  InputCheck
 }
