3911ff4ecb19c21b86da2949802c731264191b02
kengork <saracuriel.p@gmail.com>
2018-12-13 12:56:20 +0100

    refactor(component): create a 12 column grid system

--- app/renderer/ui/components/layout/sidebarLayout/index.tsx
+++ app/renderer/ui/components/layout/sidebarLayout/index.tsx
@@ -26,8 +26,7 @@ export default class SidebarLayout extends React.Component<Iprops> {
   /** render */
   // tslint:disable-next-line:prefer-function-over-method completed-docs
   public render() {
-    const gridStyles = `${styles.grid} ${this.props.collapeSidebar ? styles.collapse : ""}`
-
+  const gridStyles = `${styles.grid} ${this.props.collapeSidebar ? styles.collapse : ""}`
     return (
       <div className={gridStyles}>
         {/* TODO: Check potential refactor of existing sidebar component */}
