b9f6b5afbeb7c3d4e599e3dde4363eb0cb9df99f
kengork <saracuriel.p@gmail.com>
2019-01-14 16:28:10 +0100

    feat(ui-compo): finished marketplace tab

--- app/renderer/ui/components/main/sections/marketplace/index.tsx
+++ app/renderer/ui/components/main/sections/marketplace/index.tsx
@@ -67,20 +67,23 @@ class Marketplace extends React.Component<SectionProps> {
       <div className={grid["layout"]}>
         <header className={styles.header}>
           <div className={styles["header-text"]}>
-            <h1>Sheikah Marketplace</h1>
-            <p>
-              Hundreds of data request and smart contact templates, ready to be
-              deployed
+            <h1>
+              <span className={styles.title1}>Sheikah</span>
+              <span className={styles.title2}>Marketplace</span>
+            </h1>
+            <p className={styles.subtitle}>
+              Hundreds of data request and smart contact templates, 
+              ready to be deployed
             </p>
           </div>
         </header>
         <main className={styles.main}>
           <div className={styles["featured"]}>
-            <h2>Featured</h2>
-            <div className={styles["row"]}>
+            <h2 className={styles["section-title"]}>Featured</h2>
+            <div className={styles.row}>
               {this.state.featuredItems.map((e, i) => {
                 return (
-                  <div className={styles["item"]}>
+                  <div className={styles.item}>
                     <div className={styles["item-img"]} />
                     <div className={styles["item-text"]}>
                       <p>{e.name}</p>
@@ -102,15 +105,27 @@ class Marketplace extends React.Component<SectionProps> {
             </div>
           </div>
           <div className={styles["insurance"]}>
-            <h2>Insurance</h2>
+            <h2 className={styles["section-title"]}>Insurance</h2>
             <div className={styles.row}>
               {this.state.insuranceItems.map((e, i) => {
                 return (
                   <div key={i} className={styles.item}>
-                    <p>{e.name}</p>
-                    <p>{e.author}</p>
-                    <p>{e.rating}</p>
-                    <p>{e.hashtags.map(hashtag => hashtag)}</p>
+                    <div className={styles["item-img"]} />
+                    <div className={styles["item-text"]}>
+                      <p>{e.name}</p>
+                      <p>{e.author}</p>
+                      <br />
+                      <p>{e.rating}</p>
+                      <p className={styles["hashtag-list"]}>
+                        {e.hashtags.map(hashtag => {
+                          return (
+                            <a href="#" className={styles["hashtag-item"]}>
+                              {hashtag}
+                            </a>
+                          );
+                        })}
+                      </p>
+                    </div>
                   </div>
                 );
               })}
@@ -118,20 +133,41 @@ class Marketplace extends React.Component<SectionProps> {
           </div>
         </main>
         <nav className={styles.nav}>
-          <div className={styles["navbar-categories"]}>
-            <ul>
-              <li className={styles["navbar-item"]}>All categories</li>
-              <li className={styles["navbar-item"]}>Top Charts</li>
-              <li className={styles["navbar-item"]}>New releases</li>
-            </ul>
-          </div>
-          <div className={styles["tag-categories"]}>
+          <ul className={styles["navbar-categories"]}>
+            <li className={`${styles["navbar-item"]} ${styles["nav-border"]}`}>
+              <a href="#">All categories</a>
+            </li>
+            <li className={`${styles["navbar-item"]} ${styles["nav-border"]}`}>
+              <a href="#">Top Charts</a>
+            </li>
+            <li className={styles["navbar-item"]}>
+              <a href="#">New releases</a>
+            </li>
+          </ul>
+          <div className={`${styles["tags"]} ${styles["down"]}`}>
             <h4>Tags</h4>
-            <ul className={styles["tags"]}>
-              <li>#bet</li>
-              <li>#multisig</li>
-              <li>#oracle</li>
-              <li>#parametric</li>
+            <hr className={styles.bar} />
+            <ul className={`${styles["hashtag-list"]}`}>
+              <li>
+                <a className={`${styles["hashtag-item"]}`} href="#">
+                  #bet
+                </a>
+              </li>
+              <li>
+                <a className={`${styles["hashtag-item"]}`} href="#">
+                  #multisig
+                </a>
+              </li>
+              <li>
+                <a className={`${styles["hashtag-item"]}`} href="#">
+                  #oracle
+                </a>
+              </li>
+              <li>
+                <a className={`${styles["hashtag-item"]}`} href="#">
+                  #parametric
+                </a>
+              </li>
             </ul>
           </div>
         </nav>
