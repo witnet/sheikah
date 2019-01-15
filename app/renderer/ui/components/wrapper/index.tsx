287390bc164eafd98cd800a889cce58c096c1bbc
kengork <saracuriel.p@gmail.com>
2019-01-15 14:34:34 +0100

    style(component): travis styling fixes
    fix #539 fix #540

--- app/renderer/ui/components/wrapper/index.tsx
+++ app/renderer/ui/components/wrapper/index.tsx
@@ -1,17 +1,18 @@
-import * as React from "react";
+import * as React from "react"
 
-import { DropdownBordered } from "app/renderer/ui/components/dropdown";
+import { DropdownBordered } from "app/renderer/ui/components/dropdown"
 
-const styles = require("./style.scss");
-const receiveStyles = require("app/renderer/ui/components/main/sections/wallet/tabs/receive/style.scss") 
+const styles = require("./style.scss")
+const receiveStyles =
+  require("app/renderer/ui/components/main/sections/wallet/tabs/receive/style.scss")
 
 export interface Iprops {
-  className?: string;
-  title?: string;
-  caption?: string;
-  actions?: Array<{ text: any; onClick: any }>;
-  contentClassName?: string;
-  empty?: boolean;
+  className?: string
+  title?: string
+  caption?: string
+  actions?: Array<{ text: any; onClick: any }>
+  contentClassName?: string
+  empty?: boolean
 }
 
 /**
@@ -26,25 +27,23 @@ export default class Wrapper extends React.Component<Iprops> {
   // tslint:disable-next-line: completed-docs
   public render() {
     const contentStyles = `${!this.props.empty ? styles.content : styles.empty}
-    ${this.props.contentClassName}`;
+    ${this.props.contentClassName}`
     const actions = this.props.actions && !this.props.empty && (
       <DropdownBordered
         className={`${styles.actions} ${receiveStyles.actions}`}
         dataSource={this.props.actions}
       />
-    );
+    )
 
     return (
       <div className={`${this.props.className}`}>
         <div className={`${styles.header}`}>
           <p className={`${styles.title}`}>{this.props.title}</p>
-          {!this.props.empty && (
-            <p className={styles.caption}>{this.props.caption}</p>
-          )}
+          {!this.props.empty && (<p className={styles.caption}>{this.props.caption}</p>)}
           {actions}
         </div>
         <div className={contentStyles}>{this.props.children}</div>
       </div>
-    );
+    )
   }
 }
