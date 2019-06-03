4692e259a0c06f88ecdb39e77d2558abf16fa7ab
kengork <saracuriel.p@gmail.com>
2019-06-03 17:23:27 +0200

    feat(ui-compo): implement Button Component

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,7 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
+
 import BaseCard from '@/components/BaseCard'
 import NavigationCard from '@/components/NavigationCard'
+import Button from '@/components/Button.vue'
 
 storiesOf('BaseCard', module)
   .add('Simple', () => ({
@@ -15,4 +18,31 @@ storiesOf('BaseCard', module)
   .add('Navigation Card', () => ({
     components: { NavigationCard },
     template: '<NavigationCard title="This is a Card with Title" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.">Hello Card!</NavigationCard>',
+  }))
+
+storiesOf('Button', module)
+  .add('Default', () => ({
+    components: { Button },
+    template: '<Button type="default" @click="action">Hello Button</Button>',
+    methods: { action: action('clicked') },
+  }))
+  .add('Primary', () => ({
+    components: { Button },
+    template: '<Button type="primary" @click="action">Hello Button</Button>',
+    methods: { action: action('clicked') },
+  }))
+  .add('Danger', () => ({
+    components: { Button },
+    template: '<Button type="danger" @click="action">Hello Button</Button>',
+    methods: { action: action('clicked') },
+  }))
+  .add('Dashed', () => ({
+    components: { Button },
+    template: '<Button type="dashed" @click="action">Hello Button</Button>',
+    methods: { action: action('clicked') },
+  }))
+  .add('Positive', () => ({
+    components: { Button },
+    template: '<Button type="positive" @click="action">Hello Button</Button>',
+    methods: { action: action('clicked') },
   }))
