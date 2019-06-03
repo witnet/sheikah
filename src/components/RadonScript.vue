7c21afcec2b7306e1c8554065c64ba25dbc15ba5
tomastrg <tomastrg22@gmail.com>
2019-06-03 16:54:08 +0200

    feat(ui-compo): implement radon script component

---
+++ src/components/RadonScript.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <div v-for="(operator, index) in script" :key="operator.toString() + index">
+        <RadonOperator
+          :radOperator="operator"
+          :path="{...path, scriptIndex: index }"
+          :updateArgumentInput="updateArgumentInput"
+          :selectHashFunction="selectHashFunction"
+          :updateFilterArgument="updateFilterArgument"
+          :updateOperatorCodeSelect="updateOperatorCodeSelect"
+          :updateOperatorFilterArgument="updateOperatorFilterArgument"
+          :updateOperatorReduceArgument="updateOperatorReduceArgument"
+        />
+      </div>
+      <button @click="event => pushOperator(path)">Add operator</button>
+  </div>
+</template>
+
+<script>
+import RadonOperator from '@/components/RadonOperator'
+
+export default {
+  name: 'RadonScript',
+  components: { RadonOperator },
+  props: {
+    path: Object,
+    script: Array,
+    selectHashFunction: Function,
+    updateArgumentInput: Function,
+    updateFilterArgument: Function,
+    updateOperatorCodeSelect: Function,
+    updateOperatorFilterArgument: Function,
+    updateOperatorReduceArgument: Function,
+    pushOperator: Function,
+  },
+
+  methods: { },
+}
+</script>
