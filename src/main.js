803fa5dcb55c9f18de2d46297aff2d4a46f667de
kengork <saracuriel.p@gmail.com>
2019-06-03 18:08:17 +0200

    feat(ui-compo): implement sidebar component

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee, faWallet, faEye, faCode, faShoppingBag, faUsers, faCog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faCoffee, faWallet, faEye, faCode, faShoppingBag, faUsers, faCog)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
