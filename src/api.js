c73da54c6435720df859b329c07f02c0607d9d19
tomastrg <tomastrg22@gmail.com>
2019-06-04 17:11:39 +0200

    feat(api): add wallet api client

---
+++ src/api.js
@@ -0,0 +1,77 @@
+const RPCWebsockets = require('rpc-websockets').Client
+
+const defaultOptions = {
+  url: 'ws://localhost:11212',
+  autoconnect: true,
+  reconnect: false,
+  reconnectInterval: 5000,
+  maxReconnects: 5,
+}
+
+export class ApiClient {
+  options
+  ws
+
+  constructor (options) {
+    this.options = options || defaultOptions
+    // TODO: remove unneccessary option fields. For instance, url
+    this.ws = new RPCWebsockets(options.url, { ...options })
+  }
+
+  withOptions (opts) {
+    return new ApiClient({ ...this.options, ...opts })
+  }
+
+  async notify (method, params) {
+    return this.ws.notify(method, params)
+  }
+
+  async request (method, params) {
+    //  TODO: pass class options
+    return this.ws.call(method, params)
+  }
+
+  async subscribe (method) {
+    return this.ws.subscribe(method)
+  }
+
+  async unsubscribe (method) {
+    return this.ws.unsubscribe(method)
+  }
+
+  async open (handler) {
+    return this.ws.on('open', handler)
+  }
+
+  async on (event, handler) {
+    return this.ws.on(event, handler)
+  }
+}
+
+function callApiMethod (methodName) {
+  return (client, params) => client
+    .request(methodName, params)
+    .then(handleResponse)
+    .catch(handleError)
+}
+
+function handleResponse (response) {
+  return response
+}
+
+function handleError (error) {
+  return error
+}
+
+export const createDataRequest = callApiMethod('createDataRequest')
+export const createMnemonics = callApiMethod('createMnemonics')
+export const createWallet = callApiMethod('createWallet')
+export const generateAddress = callApiMethod('generateAddress')
+export const getTransactions = callApiMethod('getTransactions')
+export const getWalletInfos = callApiMethod('getWalletInfos')
+export const importSeed = callApiMethod('importSeed')
+export const lockWallet = callApiMethod('lockWallet')
+export const runRadRequest = callApiMethod('runRadRequest')
+export const sendDataRequest = callApiMethod('sendDataRequest')
+export const sendVTT = callApiMethod('sendVTT')
+export const unlockWallet = callApiMethod('unlockWallet')
--- src/store.js
+++ src/store.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { ApiClient } from '@/api'
+
 Vue.use(Vuex)
+// eslint-disable-next-line no-unused-vars
+const apiClient = new ApiClient()
 
 export default new Vuex.Store({
   state: {
+    mutations: {
 
-  },
-  mutations: {
+    },
+    actions: {
 
-  },
-  actions: {
-
+    },
   },
 })
