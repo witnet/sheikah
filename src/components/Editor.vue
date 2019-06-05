c4722c1f3bc028c6fa8c77969a9cb96a5fc3d4ad
tomastrg <tomastrg22@gmail.com>
2019-06-05 18:11:15 +0200

    feat(radon): implement try data request method

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
   <RadonQuery/>
-
 </div>
 </template>
 
 <script>
 import RadonQuery from '@/components/RadonQuery.vue'
+
 export default {
   name: 'Editor',
   components: {
@@ -14,7 +14,3 @@ export default {
   },
 }
 </script>
-
-<style>
-
-</style>
