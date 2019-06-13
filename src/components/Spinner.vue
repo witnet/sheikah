cf3c695c192a482326f567f03232c06e32f2ebf3
tomastrg <tomastrg22@gmail.com>
2019-06-13 15:50:27 +0200

    feat(ui-compo): implement spinner component
    close #630
    
    Co-authored-by: kengork <kengork@gmail.com>

---
+++ src/components/Spinner.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="active ? 'spinner': ''" />
+</template>
+
+<script>
+export default {
+  name: 'Spinner',
+  props: {
+    active: Boolean,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+  @import '@/styles/_colors.scss';
+
+  .spinner {
+    animation: spin 1s infinite linear;
+    border: 2px dotted $grey-5;
+    border-radius: 100%;
+    border-top: 3px solid $purple-6;
+    height: 80px;
+    width: 80px;
+    z-index: 1;
+  }
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+</style>
