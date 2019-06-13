8b8e0f56407b0c10bc61b1d27ab7659d2a08e6c5
tomastrg <tomastrg22@gmail.com>
2019-06-13 15:59:27 +0200

    fix(ui-compo): update sidebar styles and routes
    Co-authored-by: kengork <kengork@gmail.com>

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="sidebar">
       <div class="brand">
-      <router-link class="logo" to="/">
+      <router-link class="logo" to="/wallet/transaction">
         <img class="sheikah-img" src='@/resources/svg/sheikah.svg' />
       </router-link>
       </div>
@@ -10,12 +10,12 @@
        <span class="current-wallet-name">My Wallet</span>
       </div>
       <div class="link-list">
-        <router-link class="link" to="/wallet">
+        <router-link class="link" to="/wallet/transaction">
           <font-awesome-icon class="icon" icon="wallet" />
           <span class="label">Wallet</span>
         </router-link>
 
-        <router-link class="link" to="/request">
+        <router-link class="link" to="/request/editor">
           <font-awesome-icon class="icon" icon="code" />
           <span class="label">Data request</span>
         </router-link>
@@ -55,8 +55,8 @@ export default {
   },
 }
 </script>
-<style lang="scss">
 
+<style scoped lang="scss">
 @import '@/styles/theme.scss';
 @import '@/styles/app.global.scss';
 
@@ -64,131 +64,156 @@ export default {
   .sidebar {
     max-width: 75px;
   }
+
   .brand {
     clip-path: circle(60px at 0);
   }
+
   .label {
     opacity: 0;
   }
+
   .current-wallet-name {
     transform: translateX(-5px);
   }
+
   .settings {
+
     .mainnet,
     .synced {
       display: none;
     }
   }
 }
+
 .sidebar {
-    box-shadow: 0 0 20px rgba(0,0,0,0.1);
-    display: flex;
-    flex-flow: column nowrap;
-    height: 100vh;
-    z-index: 5;
-    width: 15vw;
-    font-family: 'Titillium Web'
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-flow: column nowrap;
+  height: 100vh;
+  z-index: 5;
+  width: 15vw;
+  font-family: 'Titillium Web'
+}
+
+.brand {
+  padding: 20px;
+
+  .logo {
+    width: $sidebar-logo-width;
   }
-  .brand {
-    padding: 20px;
-    .logo {
-      width: $sidebar-logo-width;
-    }
-    .sheikah-img {
-      clip-path: inset(0px 150px 0px 0px);
-    }
-    .name {
-      color: $sidebar-name-color;
-      font-size: $sidebar-name-font_size;
-      font-weight: 800;
-    }
-    .label {
-      color: $sidebar-label-color;
-      font-size: $sidebar-label-font_size;
-      font-weight: bold;
-    }
+
+  .sheikah-img {
+    clip-path: inset(0px 150px 0px 0px);
   }
-  .current-wallet {
-    background: $blue-6;
-    color: $sidebar-current_wallet-color;
-    display: flex;
-    flex-flow: row wrap;
+
+  .name {
+    color: $sidebar-name-color;
+    font-size: $sidebar-name-font_size;
+    font-weight: 800;
+  }
+
+  .label {
+    color: $sidebar-label-color;
+    font-size: $sidebar-label-font_size;
     font-weight: bold;
-    padding: 20px;
-    .current-wallet-name {
-      text-overflow: ellipsis;
-    }
   }
-  .link-list {
-    align-items: flex-start;
+}
+
+.current-wallet {
+  background: $blue-6;
+  color: $sidebar-current_wallet-color;
+  display: flex;
+  flex-flow: row wrap;
+  font-weight: bold;
+  padding: 20px;
+
+  .current-wallet-name {
+    text-overflow: ellipsis;
+  }
+}
+
+.link-list {
+  align-items: flex-start;
+  display: flex;
+  flex-flow: column nowrap;
+  margin-top: 10px;
+  width: 100%;
+
+  .link {
+    align-items: center;
+    border-left: $sidebar-inactive-border;
+    color: $sidebar-link-color;
     display: flex;
-    flex-flow: column nowrap;
-    margin-top: 10px;
+    flex-flow: row nowrap;
+    font-size: $sidebar-link-font_size;
+    font-weight: $sidebar-link-font_weight;
+    padding: 15px 8px;
+    text-decoration: none;
     width: 100%;
-    .link {
-      align-items: center;
-      border-left: $sidebar-inactive-border;
-      color: $sidebar-link-color;
-      display: flex;
-      flex-flow: row nowrap;
-      font-size: $sidebar-link-font_size;
-      font-weight: $sidebar-link-font_weight;
-      padding: 15px 8px;
-      text-decoration: none;
-      width: 100%;
-      text-align: left;
-      &.router-link-active{
-        border-left: $sidebar-active-border;
-        color: $sidebar-active-color;
-      }
-      .icon {
-        margin-right: 20px;
-        margin-left: 10px;
-        width: 20px;
-      }
-      .label {
-        width: 100%;
-      }
-    }
-    .active {
+    text-align: left;
+
+    &.router-link-active {
       border-left: $sidebar-active-border;
       color: $sidebar-active-color;
     }
+
+    .icon {
+      margin-right: 20px;
+      margin-left: 10px;
+      width: 20px;
+    }
+
+    .label {
+      width: 100%;
+    }
   }
-  .settings {
-    align-items: center;
-    border-top: $sidebar-settings-border;
+
+  .active {
+    border-left: $sidebar-active-border;
+    color: $sidebar-active-color;
+  }
+}
+
+.settings {
+  align-items: center;
+  border-top: $sidebar-settings-border;
+  display: flex;
+  flex-flow: row nowrap;
+  font-size: $icon-settings-font_size;
+  justify-content: space-between;
+  margin-top: auto;
+  padding: 15px;
+
+  .settings-icon {
+    color: $sidebar-settings-icon-color;
+    cursor: pointer;
+    font-size: $sidebar-settings-icon-size;
+  }
+
+  .net-status {
+    font-size: $icon_status-font_size;
     display: flex;
-    flex-flow: row nowrap;
-    font-size: $icon-settings-font_size;
-    justify-content: space-between;
-    margin-top: auto;
-    padding: 15px;
-    .settings-icon {
-      color: $sidebar-settings-icon-color;
-      cursor: pointer;
-      font-size: $sidebar-settings-icon-size;
+    align-items: center;
+
+    .dot {
+      height: 10px;
+      width: 10px;
+      background-color: $yellow-5;
+      border-radius: 50%;
+      display: inline-block;
     }
-    .net-status {
-      font-size: $icon_status-font_size;
-      display: flex;
-      align-items: center;
-      .dot {
-        height: 10px;
-        width: 10px;
-        background-color: $yellow-5;
-        border-radius: 50%;
-        display: inline-block;
-      }
-      .mainnet {
-        color: $sidebar-mainnet-color;
-        font-weight: $sidebar-mainnet-font_weight;
-      }
-      .synced {
-        color: $sidebar-synced-color;
-        font-weight: $sidebar-synced-font_weight;
-        padding: 0.5em;
-      }
+
+    .mainnet {
+      color: $sidebar-mainnet-color;
+      font-weight: $sidebar-mainnet-font_weight;
+    }
+
+    .synced {
+      color: $sidebar-synced-color;
+      font-weight: $sidebar-synced-font_weight;
+      padding: 0.5em;
     }
   }
+}
 </style>
