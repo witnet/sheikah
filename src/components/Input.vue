67b9c3bdfa2a8525bf07560d9f8835f7af4062b5
tomastrg <tomastrg22@gmail.com>
2019-06-13 15:45:23 +0200

    feat(ui-compo): implement input component
    close #621
    
    Co-authored-by: kengork <kengork@gmail.com>

---
+++ src/components/Input.vue
@@ -0,0 +1,40 @@
+<template>
+  <textarea
+    @input="$emit('update:model', $event.target.value)"
+    v-value="model"
+    :class="type === 'big' ? type : '' "
+  />
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  props: {
+    type: String,
+    model: {
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/theme.scss';
+
+.big {
+  border: $input_big-border;
+  border-radius: $input_big-border-radius;
+  min-height: 80px;
+  transition: all 0.1s ease;
+  width: $input_big-width;
+
+  &:hover,
+  &:focus {
+    border: $input_big-hover-border;
+    box-shadow: $input_big-hover-box-shadow;
+    outline: 0;
+    outline-color: transparent;
+    outline-style: none;
+  }
+}
+</style>
