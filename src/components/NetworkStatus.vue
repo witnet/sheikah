94fff1e95d432afd697ebba6d8e5d2797ce28d4d
tomastrg <tomastrg22@gmail.com>
2019-06-18 13:42:17 +0200

    feat(ui-compo): implement NetworkStatus

---
+++ src/components/NetworkStatus.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="network-status">
+    <span class="label">NETWORK STATUS</span>
+    <span class="status">{{ status.toUpperCase() }}</span>
+    <DotIndicator :status="status" />
+  </div>
+</template>
+
+<script>
+import DotIndicator from '@/components/DotIndicator'
+export default {
+  name: 'NetworkStatus',
+  props: {
+    status: String,
+  },
+  components: {
+    DotIndicator,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/theme.scss';
+@import '@/styles/app.global.scss';
+
+.network-status {
+  font-size: $icon_status-font_size;
+  display: flex;
+  align-items: center;
+
+  .label {
+    color: $sidebar-mainnet-color;
+    font-weight: $sidebar-mainnet-font_weight;
+  }
+
+  .status{
+    color: $sidebar-synced-color;
+    font-weight: $sidebar-synced-font_weight;
+    padding: 0.5em;
+  }
+}
+</style>
