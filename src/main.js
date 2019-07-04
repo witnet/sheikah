328165b0e6b4a90d99e8cf56abd1c4d5c274ceb7
tomastrg <tomastrg22@gmail.com>
2019-07-04 17:05:54 +0200

    feat(views): connect welcome back flow with wallet

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import { WalletApi } from './api'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee, faWallet, faEye, faCode, faShoppingBag, faUsers, faCog, faAngleRight, faAngleLeft, faCircle, faSortDown } from '@fortawesome/free-solid-svg-icons'
@@ -14,8 +15,24 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app')
+const walletApi = new WalletApi()
+
+const isSocketReady = setInterval(checkSocketStatus, 1)
+
+function checkSocketStatus () {
+  if (walletApi.client.ws.ready) {
+    runApp()
+  }
+}
+
+function runApp () {
+  clearInterval(isSocketReady)
+
+  store.$walletApi = walletApi
+
+  new Vue({
+    router,
+    store,
+    render: h => h(App),
+  }).$mount('#app')
+}
