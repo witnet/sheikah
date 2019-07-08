419edb0167ac39c567a6574964dd77e438230d8e
tomastrg <tomastrg22@gmail.com>
2019-07-08 18:19:48 +0200

    feat(ui-compo): implement send transaction component

--- src/components/Send.vue
+++ src/components/Send.vue
@@ -1,20 +1,94 @@
 <template>
-  <div>
-    <SwitchCheckbox />
-  </div>
+<div class="send">
+  <BaseCard class="card" title="NEW TRANSACTION">
+    <div class="row">
+    <div class="label">
+      <label class="" for>Address</label>
+      </div>
+      <Input v-model="address" placeholder="Recipient address" />
+    </div>
+
+    <div class="row">
+    <div class="label">
+      <label class="" for>Label</label>
+      </div>
+      <Input v-model="label" />
+    </div>
+
+    <div class="row">
+      <div class="label">
+        <label class="" for>Amount</label>
+      </div>
+      <InputNumber v-model="amount" />
+    </div>
+    <div class="row">
+      <div class="label">
+        <label class="" for>Fee</label>
+      </div>
+      <Select v-model="fee" :options="options" />
+    </div>
+
+    <Button class="button" :onClick="() => 0" type="primary">Sign and send</Button>
+  </BaseCard>
+</div>
 </template>
 
 <script>
-import SwitchCheckbox from './SwitchCheckbox'
+import BaseCard from './BaseCard'
+import Button from './Button'
+import Input from './Input'
+import InputNumber from './InputNumber'
+import Select from './Select'
 
 export default {
   name: 'send',
   components: {
-    SwitchCheckbox,
+    BaseCard,
+    Button,
+    Input,
+    InputNumber,
+    Select,
+  },
+  data () {
+    return {
+      address: '',
+      label: '',
+      amount: 0,
+      fee: '',
+      options: [
+        { value: 79, primaryText: 'High', secondaryText: '79 uWit/B' },
+        { value: 59, primaryText: 'Medium', secondaryText: '59 uWit/B' },
+        { value: 39, primaryText: 'Low', secondaryText: '39 uWit/B' },
+      ],
+    }
   },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.send {
+  padding: 32px;
 
+  .card {
+    max-width: 1000px;
+    min-width: 500px;
+    .row {
+      display: flex;
+      align-items: center;
+      margin-bottom: 16px;
+      max-width: 600px;
+
+      .label {
+        min-width: 64px;
+        margin-right: 24px;
+        text-align: right;
+      }
+    }
+
+    .button {
+      margin-left: 96px;
+      margin-top: 24px;
+    }
+  }
+}
 </style>
