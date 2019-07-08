576858d6cbcfe2bf6f4576d18a9286c2a4140463
tomastrg <tomastrg22@gmail.com>
2019-07-08 18:16:48 +0200

    feat(style): update basic components style

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -13,7 +13,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '@/styles/_colors.scss';
 @import '@/styles/theme.scss';
 @import '@/styles/_colors.scss';
