b660cafb3ce5036fbdb93e6ba58350d7dac71e7e
María Gabaldón Rico <mariag@stampery.co>
2019-07-17 16:34:21 +0200

    chore: sort imports and limit max length

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
+import VueQrcode from '@chenfengyuan/vue-qrcode'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './fontAwesome'
 import './plugins/element.js'
 import { WalletApi } from './api'
-import VueQrcode from '@chenfengyuan/vue-qrcode'
-import './fontAwesome'
 
 Vue.component(VueQrcode.name, VueQrcode)
 Vue.config.productionTip = false
