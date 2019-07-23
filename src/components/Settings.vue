fa80bfa3578ddf6397d9dc90b67a6b055c1d7967
María Gabaldón Rico <mariag@stampery.co>
2019-07-23 15:05:10 +0200

    feat(ui-compo): add settings component

---
+++ src/components/Settings.vue
@@ -0,0 +1,68 @@
+<template>
+<div>
+    <el-dropdown @command="handleCommand">
+        <el-button split-button type="primary" >
+        <font-awesome-icon class="icon" icon="cog"/>
+        </el-button>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item v-for="(setting, index) in settings" :key="setting.label" :command="index">
+            {{setting.label}}
+          </el-dropdown-item>
+        </el-dropdown-menu>
+    </el-dropdown>
+</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+    name: 'Settings',
+    props: {
+      settings: Array,
+    },
+    methods: {
+      handleCommand(index) {
+        this.settings[index].action()
+      }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+.el-dropdown-menu__item {
+  padding: 15px;
+  margin-bottom: 3px;
+  font-weight: bold;
+  color:#909090;
+  &:not(.is-disabled):hover{
+    color: #1a6cfb;
+    font-weight: bold;
+  }
+}
+.el-button {
+  cursor: pointer;
+  background: none;
+  color: inherit;
+  border: none;
+}
+.icon{
+  color: inherit;
+}
+.el-button, .el-dropdown-selfdefine :hover {
+  cursor: pointer;
+  background: none;
+  color: inherit;
+  border: none;
+ 
+  &:hover .icon{
+  background: none;
+  border: none;
+  }
+}
+.el-button--primary:focus, .el-button--primary:hover, .el-button--primary:active{
+  background: none;
+  color: inherit;
+}
+</style>
