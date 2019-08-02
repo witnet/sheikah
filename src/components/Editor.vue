c5b8205d4167efae91db068d15241426eff1bd87
María Gabaldón Rico <mariag@stampery.co>
2019-08-02 10:17:13 +0200

    feat(ui-compo): implement toolbar for editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -23,5 +23,27 @@ export default {
       consensus: state => state.rad.radRequest.consensus,
     }),
   },
+  data(){
+    return{
+      tabs: [
+        {icon: 'Redo', 
+        action: 'redo changes' ,
+        class:'end'
+        },
+        {icon: 'Undo', 
+        action: 'Undo changes',
+        class:'end'
+        },
+        {icon: 'Save', 
+        action: 'Save changes',
+        class:'end'
+        },
+        {icon: 'Try data request', 
+        action: 'Try data request',
+        class:'end'
+        }
+      ],
+    }
+  }
 }
 </script>
