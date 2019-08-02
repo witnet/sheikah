67f961a2e25ac490ba359073f41689c833097ae8
María Gabaldón Rico <mariag@stampery.co>
2019-08-02 10:17:13 +0200

    feat(ui-compo): implement toolbar for editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,19 +1,21 @@
 <template>
 <div>
-  <RadonStage class="stage" :retrieve="retrieve" />
-  <RadonStage class="stage" :aggregate="aggregate" />
-  <RadonStage class="stage" :consensus="consensus" />
+  <ToolBar :tabs="tabs" />
+  <RadonQuery/>
 </div>
 </template>
 
 <script>
+
 import { mapState } from 'vuex'
 import RadonStage from '@/components/RadonStage.vue'
+import ToolBar from '@/components/ToolBar.vue'
 
 export default {
   name: 'Editor',
   components: {
     RadonStage,
+    ToolBar,
   },
   computed: {
     ...mapState({
@@ -22,5 +24,27 @@ export default {
       consensus: state => state.rad.radRequest.consensus,
     }),
   },
+  data(){
+    return{
+      tabs: [
+        {icon: 'Redo', 
+        action: 'redo changes' ,
+        class:'end'
+        },
+        {icon: 'Undo', 
+        action: 'Undo changes',
+        class:'end'
+        },
+        {icon: 'Save', 
+        action: 'Save changes',
+        class:'end'
+        },
+        {icon: 'Try data request', 
+        action: 'Try data request',
+        class:'end'
+        }
+      ],
+    }
+  }
 }
 </script>
