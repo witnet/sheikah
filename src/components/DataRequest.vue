6c0b4a1e7ea67730a07ed84da33cae720e54f93f
María Gabaldón Rico <mariag@stampery.co>
2019-08-07 11:29:04 +0200

    fix(ui-compo): add try data request method

--- src/components/DataRequest.vue
+++ src/components/DataRequest.vue
@@ -16,7 +16,6 @@ export default {
   data () {
     return {
       tabs: [
-        { name: 'Editor', link: '/request/editor' },
         { name: 'Templates', link: '/request/templates' },
       ],
     }
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,6 @@
 <template>
 <div>
-  <ToolBar :tabs="tabs" />
-  <RadonQuery/>
+  <ToolBar/>
 </div>
 </template>
 
@@ -24,27 +23,5 @@ export default {
       consensus: state => state.rad.radRequest.consensus,
     }),
   },
-  data(){
-    return{
-      tabs: [
-        {icon: 'Redo', 
-        action: 'redo changes' ,
-        class:'end'
-        },
-        {icon: 'Undo', 
-        action: 'Undo changes',
-        class:'end'
-        },
-        {icon: 'Save', 
-        action: 'Save changes',
-        class:'end'
-        },
-        {icon: 'Try data request', 
-        action: 'Try data request',
-        class:'end'
-        }
-      ],
-    }
-  }
 }
 </script>
