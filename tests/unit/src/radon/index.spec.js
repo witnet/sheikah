342a20440360dd2fb5a2b427b4d776417f12e3b1
María Gabaldón Rico <mariag@stampery.co>
2019-08-20 18:10:28 +0200

    feat(radon): implement radon2markup and markup2radon

--- tests/unit/src/radon/index.spec.js
+++ tests/unit/src/radon/index.spec.js
@@ -1,57 +1,23 @@
 import { RadonMarkupInterpreter } from '@/radon'
+const { drMarkup, drMir } = require('./dataset')
 
 describe('RadonMarkupInterpreter', () => {
   it('converts MIR to markup correctly', () => {
-    const mir = {
-      notBefore: 1669852800,
-      retrieve: [
-        {
-          url: 'https://api.coindesk.com/v1/bpi/currentprice.json',
-          script: [118, 69, 116, [97, 'bpi'], 116, [97, 'USD'], 116, [97, 'rate_float'], 114],
-        },
-        {
-          url: 'https://blockchain.info/ticker',
-          script: [117, 69, 116, [97, 'USD'], 116, [97, '15m'], 114],
-        },
-        {
-          url: 'https://www.bitstamp.net/api/ticker/',
-          script: [117, 69, 116, [97, 'last'], 114],
-        },
-      ],
-      aggregate: { script: [84, [83, 5, 2], [87, 3]] },
-      tally: { script: [84, [83, 5, 1.5], [87, 3]] },
-    }
+    const rmi = new RadonMarkupInterpreter(drMir.radRequest)
 
-    const rmi = new RadonMarkupInterpreter(mir)
-    const { dr } = require('./radonMarkup')
-
-    expect(rmi.getMarkup()).toMatchObject(dr)
+    expect(rmi.getMarkup()).toMatchObject(drMarkup)
   })
 
-  it('update correctly by id', () => {
-    const mir = {
-      notBefore: 1669852800,
-      retrieve: [
-        {
-          url: 'https://api.coindesk.com/v1/bpi/currentprice.json',
-          script: [117, 69, 116, [97, 'bpi'], 116, [97, 'USD'], 116, [97, 'rate_float'], 114],
-        },
-        {
-          url: 'https://blockchain.info/ticker',
-          script: [117, 69, 116, [97, 'USD'], 116, [97, '15m'], 114],
-        },
-        {
-          url: 'https://www.bitstamp.net/api/ticker/',
-          script: [117, 69, 116, [97, 'last'], 114],
-        },
-      ],
-      aggregate: { script: [84, [83, 5, 2], [87, 3]] },
-      tally: { script: [84, [83, 5, 1.5], [87, 3]] },
-    }
-
-    const rmi = new RadonMarkupInterpreter(mir)
-    const { updatedDr1 } = require('./radonMarkup')
+  it.only('update correctly by id', () => {
+    const rmi = new RadonMarkupInterpreter(drMir.radRequest)
     rmi.updateElement(1, 'hash')
-    expect(rmi.getMarkup()).toMatchObject(updatedDr1)
+    expect(rmi.getMarkup()).toMatchObject(drMarkup)
+  })
+
+  it('markupToRadon', () => {
+    const { drMir } = require('./dataset')
+    const rmi = new RadonMarkupInterpreter(drMir.radRequest)
+
+    expect(rmi.getMir()).toMatchObject(drMir.radRequest)
   })
 })
