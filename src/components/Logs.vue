7944b70d6da34a33ce77fe8ea8609ff7bcfb8e6f
María Gabaldón Rico <mariag@stampery.co>
2019-12-02 17:06:06 +0100

    feat(compo): implement tab-bar for console component

---
+++ src/components/Logs.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="logs-container">
+    <div class="log">
+      Log
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logs',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/_colors.scss';
+@import '@/styles/theme.scss';
+
+.logs-container {
+  width: 100%;
+  height: 20vh;
+  overflow-y: scroll;
+  background-color: $black;
+  text-align: center;
+  text-justify: center;
+  .log {
+    color: red;
+    padding-left: 50px;
+    display: block;
+    display: flex;
+    justify-items: left;
+  }
+}
+</style>
