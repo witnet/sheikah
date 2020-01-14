179dbaa7165c863bd1fc3af6a9b7c44ec6be2c29
María Gabaldón Rico <mariag@stampery.co>
2020-01-14 10:53:31 +0100

    fix: dropdown style

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,10 +1,7 @@
 <template>
-  <div>
+  <div class="button">
     <el-dropdown class="dropdown" @command="handleCommand">
-      <div class="button">
-        <font-awesome-icon class="icon" :class="[color]" icon="cog" />
-        <p v-if="showText" class="text" :class="[color]">{{ text }}</p>
-      </div>
+      <font-awesome-icon class="icon" :class="[color]" icon="cog" />
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item
           v-for="(setting, index) in settings"
@@ -15,6 +12,7 @@
         </el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
+    <p v-if="showText" class="text" :class="[color]">{{ text }}</p>
   </div>
 </template>
 
@@ -47,35 +45,69 @@ export default {
 @import '@/styles/_colors.scss';
 @import '@/styles/theme.scss';
 
-.dropdown {
-  padding: 4px;
-  .button {
-    display: flex;
-    align-items: center;
-    cursor: pointer;
+.button {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  .icon {
+    font-size: 18px;
 
-    .icon {
-      font-size: 18px;
-
-      &.light {
-        color: white;
-      }
-      &.dark {
-        color: $grey-5;
-      }
+    &.light {
+      color: white;
     }
-    .text {
-      margin-left: 8px;
-      font-size: 18px;
-
-      &.light {
-        color: white;
-      }
-
-      &.dark {
-        color: $grey-5;
-      }
+    &.dark {
+      color: $grey-5;
     }
   }
+  .text {
+    margin-left: 8px;
+    font-size: 18px;
+
+    &.light {
+      color: white;
+    }
+
+    &.dark {
+      color: $grey-5;
+    }
+  }
+}
+
+.el-dropdown-menu {
+  padding: 8px 0;
+}
+.el-dropdown-menu__item {
+  padding: 0px 8px;
+  font-weight: bold;
+  color: $grey-3;
+  &:not(.is-disabled):hover {
+    color: $blue-6;
+    font-weight: bold;
+  }
+}
+.el-button {
+  padding: 8px 16px 8px 8px;
+  cursor: pointer;
+  background: none;
+  color: inherit;
+  border: none;
+}
+.el-button,
+.el-dropdown-selfdefine :hover {
+  cursor: pointer;
+  background: none;
+  color: inherit;
+  border: none;
+
+  &:hover .icon {
+    background: none;
+    border: none;
+  }
+}
+.el-button--primary:focus,
+.el-button--primary:hover,
+.el-button--primary:active {
+  background: none;
+  color: inherit;
 }
 </style>
