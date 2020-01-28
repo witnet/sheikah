ea97583042f86540145d2accbc0bb5367964809e
tommytrg <tomastrg22@gmail.com>
2020-01-28 18:16:50 +0100

    test: add a utility function to create and unlock wallet

--- tests/e2e/utils.js
+++ tests/e2e/utils.js
@@ -1,3 +1,5 @@
+import { WalletApi } from '../../src/api'
+
 export function createSelection(field, start, end) {
   if (field.createTextRange) {
     var selRange = field.createTextRange()
@@ -13,4 +15,36 @@ export function createSelection(field, start, end) {
   }
   field.focus()
   return field[0].innerHTML
+}
+
+export async function createAndUnlockWallet() {
+  const api = new WalletApi()
+  const promise = new Promise((resolve, reject) => {
+    api.client.ws.on('open', async () => {
+      const createWallet = await api.createWallet({
+        caption: 'test',
+        name: 'test',
+        password: 'password',
+        seedData: 'soldier design onion below soldier judge lock discover load hour option atom',
+        seedSource: 'mnemonics',
+      })
+
+      const openWallet = await api.unlockWallet({
+        walletId: createWallet.result.walletId,
+        password: 'password',
+      })
+
+      resolve({ ...openWallet.result, walletId: createWallet.result.walletId })
+    })
+  })
+
+  const openWallet = await promise
+
+  cy.visit('/')
+
+  const win = await cy.window()
+  const vueInstance = win.vm
+
+  vueInstance.$store.state.wallet.sessionId = openWallet.sessionId
+  vueInstance.$store.state.wallet.walletId = openWallet.walletI
 }
