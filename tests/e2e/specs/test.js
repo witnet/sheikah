d7195c2ccde18834f44492bfcac8e6e98f83d715
tommytrg <tomastrg22@gmail.com>
2020-01-28 18:11:35 +0100

    test: simplify sanity test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe.skip('My First Test', () => {
+describe.skip('Sanity test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('.header', 'Ey, listen!')
   })
 })
