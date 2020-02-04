cdd22a548364ba2c71b5529737687d5e4f71a6fd
tommytrg <tomastrg22@gmail.com>
2020-02-04 12:28:38 +0100

    test(e2e): add vuex and create wallet cypress commands

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+// Get vuex instance from window
+Cypress.Commands.add('vuex', () => {
+  cy.visit('/')
+  cy.window().its('vm.$store')
+})
+
+// Create a wallet and set it in vuex store
+Cypress.Commands.add('createAndUnlockWallet', () => {
+  cy.vuex().invoke('dispatch', 'createWallet', {
+    sourceType: 'mnemonics',
+    password: 'password',
+    mnemonics: 'soldier design onion below soldier judge lock discover load hour option atom',
+  })
+  cy.wait(2000)
+})
