edea99befa54744d0efedec7ede2c68cbcdab56e
tommytrg <tomastrg22@gmail.com>
2020-02-13 15:31:19 +0100

    ref: remove unnecessary dependencies

--- src/stories/stage.stories.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { storiesOf } from '@storybook/vue'
-import RadonStage from '@/components/RadonStage'
-
-storiesOf('RadonStage', module).add('Retrieve', () => ({
-  components: { RadonStage },
-  template: '<RadonStage :retrieve="[{script: []}]"/>',
-}))
