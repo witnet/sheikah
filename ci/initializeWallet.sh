669be1a2d148ce0fe44ed13d8630ca6018ddbe5e
tmpolaczyk <44604217+tmpolaczyk@users.noreply.github.com>
2020-03-02 17:59:22 +0100

    chore: retry curl and wget if github is down

--- ci/initializeWallet.sh
+++ ci/initializeWallet.sh
@@ -2,7 +2,17 @@
 
 echo 'Downloading latest witnet node release...'
 
-wget $(curl -s https://api.github.com/repos/witnet/witnet-rust/releases/latest | grep 'browser_' | cut -d\" -f4)
+# Retry curl until success
+while URL=$(curl -s https://api.github.com/repos/witnet/witnet-rust/releases/latest | grep 'browser_' | cut -d\" -f4); [ "$URL" = "" ]; do
+    echo 'Github is down, retrying in 10s'
+    sleep 10s
+done
+
+# Retry wget until success
+# https://stackoverflow.com/a/30986740
+while true; do
+    wget -T 15 -c $URL && break
+done
 
 tar -zxvf witnet-*-x86_64-unknown-linux-gnu.tar.gz
 
