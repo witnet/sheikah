081335a00ab35ed0e7b05755e25ecbfe926fa726
María Gabaldón Rico <mariag@stampery.co>
2020-03-11 12:47:46 +0100

    feat: save claiming info

---
+++ src/components/claiming/WalletSeedValidation.vue
@@ -0,0 +1,113 @@
+<template>
+  <NavigationCard
+    data-test="header-4"
+    class="wallet-seed-validation"
+    title="Confirm your seed phrase"
+    previousText="Back"
+    nextText="Next"
+    :previousStep="previousStep"
+    :nextStep="nextStep"
+    ref="navCard"
+  >
+    <p>
+      Please type your 12 word seed phrase exactly as it was shown to you on the previous screen.
+      This step is to confirm that you have copied your seed phrase correctly.
+    </p>
+    <Input type="big" class="seed" v-model="seed" v-on:go-next="nextStep" />
+    <p data-test="mnemonics-error-alert" class="match-error" v-if="mnemonicsError">
+      Mnemonics must match
+    </p>
+    <p class="paragraph">
+      Please ensure you do not add any extra spaces between words or at the beginning or end of the
+      phrase.
+    </p>
+  </NavigationCard>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+import NavigationCard from '@/components/card/NavigationCard'
+import Input from '@/components/Input.vue'
+
+export default {
+  name: 'WalletSeedValidation',
+  components: {
+    NavigationCard,
+    Input,
+  },
+  data() {
+    return {
+      seed: '',
+      showError: '',
+    }
+  },
+  computed: {
+    ...mapState({
+      mnemonics: state => state.wallet.mnemonics,
+      mnemonicsError: state => {
+        return state.wallet.errors.mnemonics
+      },
+      validatedMnemonics: state => state.wallet.validatedMnemonics,
+    }),
+  },
+  methods: {
+    validateForm() {
+      this.$store.commit('validateMnemonics', {
+        seed: this.seed,
+        mnemonics: this.mnemonics,
+      })
+    },
+    nextStep() {
+      this.validateForm()
+      if (this.validatedMnemonics) {
+        if (this.mnemonicsError) {
+          this.clearError(this.mnemonicsError.name)
+        }
+        this.$router.push('/claiming/encryption-pass')
+      }
+    },
+    clearError(errorName) {
+      this.$store.commit('clearError', { error: errorName })
+    },
+    previousStep() {
+      this.$router.push('/claiming/seed-backup')
+    },
+  },
+  beforeDestroy() {
+    if (this.mnemonicsError) {
+      this.clearError(this.mnemonicsError.name)
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import '@/styles/theme.scss';
+@import '@/styles/_colors.scss';
+
+.content {
+  padding: 40px;
+}
+
+.seed {
+  align-items: center;
+  border: $input_big-border;
+  border-radius: $input_big-border-radius;
+  box-sizing: border-box;
+  color: $input_big-color;
+  display: inline-flex;
+  font-size: 16px;
+  line-break: auto;
+  line-height: 1.5em;
+  margin: 32px 0;
+  padding: 16px;
+  width: 100%;
+  margin-top: 40px;
+}
+.match-error {
+  color: $red-0;
+}
+.paragraph {
+  margin-top: 16px;
+}
+</style>
