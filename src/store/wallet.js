88303496191c2145a0a725cef0fcfd46205540a7
María Gabaldón Rico <mariag@stampery.co>
2020-03-20 10:52:49 +0100

    feat: on claiming process completed redirect to unlock wallet

--- src/store/wallet.js
+++ src/store/wallet.js
@@ -123,7 +123,6 @@ export default {
     },
     setClaimingInfo(state, { info }) {
       Object.assign(state, { claimingFileInfo: info })
-      console.log(state.claimingFileInfo)
     },
     setClaimingState(state, { completed }) {
       Object.assign(state, { claimingProcessState: completed })
