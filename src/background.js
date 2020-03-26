5ec8f924a28ed35c437a31e624946169f5d4e28a
tommytrg <tomastrg22@gmail.com>
2020-03-26 10:36:50 +0100

    fix: update how icon is set for bundle

--- src/background.js
+++ src/background.js
@@ -48,7 +48,7 @@ function createWindow() {
     height: 600,
     minWidth: 800,
     minHeight: 600,
-    icon: path.join(__static, 'icon.ico'),
+    icon: path.join(__static, 'icon.png'),
     webPreferences: {
       nodeIntegration: true,
     },
