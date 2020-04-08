29a0d7d32cf92fd5e5087a94b5315fa7ffbd3c40
tommytrg <tomastrg22@gmail.com>
2020-04-08 12:07:22 +0200

    ref(main): move layout structure to layoutSidebar

---
+++ src/components/LayoutSidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="layout" >
+    <Sidebar />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+
+export default {
+  name: 'LayoutSidebar',
+  components: {
+    Sidebar,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Titillium+Web:300,300i,400,400i,600,600i&display=swap');
+
+.layout {
+  display: grid;
+  grid-template: 1fr / auto 1fr;
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="layout" data-test="home">
-    <Sidebar />
-    <router-view class="main"></router-view>
-  </div>
+  <LayoutSidebar data-test="main">
+    <router-view></router-view>
+  </LayoutSidebar>
 </template>
 
 <script>
-import Sidebar from '@/components/Sidebar.vue'
 import { mapActions } from 'vuex'
+import LayoutSidebar from '@/components/LayoutSidebar'
 
 export default {
   name: 'Main',
   components: {
-    Sidebar,
+    LayoutSidebar,
   },
   created() {
     this.pollData()
@@ -42,21 +41,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=Titillium+Web:300,300i,400,400i,600,600i&display=swap');
-
-.layout {
-  display: flex;
-  position: relative;
-
-  .main {
-    height: 100vh;
-    overflow-y: auto;
-    width: 100%;
-  }
-  .alert {
-    margin: 0px;
-  }
-}
-</style>
