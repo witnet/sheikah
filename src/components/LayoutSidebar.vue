7a8dff994f59976bb09ee6f6beeb2fc6a03c96af
tommytrg <tomastrg22@gmail.com>
2020-04-13 16:59:39 +0200

    feat: implement LayoutSidebar component

--- src/components/LayoutSidebar.vue
+++ src/components/LayoutSidebar.vue
@@ -1,26 +1,56 @@
-<template>
-  <div class="layout" >
-    <Sidebar />
-    <slot></slot>
-  </div>
-</template>
-
 <script>
 import Sidebar from '@/components/Sidebar.vue'
-
+// Layout that displays the sidebar next to the content provided
 export default {
   name: 'LayoutSidebar',
   components: {
     Sidebar,
   },
+  render(createElement) {
+    return createElement(
+      'div',
+      {
+        attrs: {
+          class: 'layout',
+        },
+      },
+      [
+        createElement(Sidebar),
+        createElement(
+          'div',
+          {
+            attrs: {
+              class: 'content',
+            },
+          },
+          /** @slot Default slot will be rendered next to the sidebar */
+          this.$slots.default
+        ),
+      ]
+    )
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=Titillium+Web:300,300i,400,400i,600,600i&display=swap');
-
 .layout {
   display: grid;
   grid-template: 1fr / auto 1fr;
+
+  .content {
+    padding: 25px;
+  }
 }
 </style>
+
+<docs>
+### Example
+
+```jsx
+  <LayoutSidebar>
+    <div :style="{ background: '#E6D4F7', height: '100%' }">
+      <p>I'm next to the sidebar</p>
+    </div>
+  </LayoutSidebar>
+```
+</docs>
---
+++ tests/unit/src/components/LayoutSidebar.spec.js
@@ -0,0 +1,24 @@
+import LayoutSidebar from '@/components/LayoutSidebar'
+import Sidebar from '@/components/Sidebar'
+
+describe('LayoutSidebar.vue', () => {
+  it('render the slot', () => {
+    const slotContent = '<div>Test</div>'
+    const { element } = shallowMount(LayoutSidebar, {
+      slots: {
+        default: slotContent,
+      },
+    })
+    expect(element.innerHTML).toContain(slotContent)
+  })
+
+  it('render the siderbar', () => {
+    const slotContent = '<div>Test</div>'
+    const wrapper = shallowMount(LayoutSidebar, {
+      slots: {
+        default: slotContent,
+      },
+    })
+    expect(wrapper.contains(Sidebar)).toBe(true)
+  })
+})
