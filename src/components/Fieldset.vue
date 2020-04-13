492dc0003198212c7c252cce34821af6e4fab2eb
tommytrg <tomastrg22@gmail.com>
2020-04-13 17:47:25 +0200

    feat: implement Fieldset component

---
+++ src/components/Fieldset.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="fieldset">
+    <header class="header">
+      <p class="title">{{ title }}</p>
+      <p v-if="subtitle" class="subtitle">{{ subtitle }}</p>
+      <p v-if="helper" class="helper">{{ helper }}</p>
+    </header>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fieldset',
+  props: {
+    /**
+     * Main title
+     */
+    title: {
+      type: String,
+      required: true,
+    },
+    /**
+     * Secondary title
+     */
+    subtitle: {
+      type: String,
+      required: false,
+    },
+    /**
+     * Terciary title
+     */
+    helper: {
+      type: String,
+      required: false,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+// TODO: change colors for variables before merge
+.fieldset {
+  .header {
+    margin-bottom: 25px;
+    display: flex;
+    .title {
+      color: #444258;
+      font-size: 16px;
+      font-weight: bold;
+    }
+
+    .subtitle {
+      color: #8280a4;
+      font-size: 14px;
+      margin-left: 8px;
+    }
+
+    .helper {
+      color: #444258;
+      font-size: 10px;
+      opacity: 50%;
+      font-style: italic;
+      margin-left: 25px;
+      align-self: center;
+    }
+  }
+}
+</style>
+
+<docs>
+### Example
+
+```jsx
+  <Fieldset
+    title="title"
+    subtitle="subtitle"
+    helper="helper"
+  >
+    <div :style="{ width: '200px', height: '100px' }">Content</div>
+  </Fieldset>
+
+```
+</docs>
---
+++ tests/unit/src/components/Fieldset.spec.js
@@ -0,0 +1,76 @@
+import Fieldset from '@/components/Fieldset'
+
+describe('Fieldset.vue', () => {
+  it('should render the slot', () => {
+    const slotContent = '<div>Test</div>'
+    const { element } = shallowMount(Fieldset, {
+      propsData: {
+        title: 'title',
+      },
+      slots: {
+        default: slotContent,
+      },
+    })
+    expect(element.innerHTML).toContain(slotContent)
+  })
+
+  describe('should render title props', () => {
+    it('title', () => {
+      const slotContent = '<div>Test</div>'
+      const wrapper = shallowMount(Fieldset, {
+        propsData: {
+          title: 'title',
+        },
+        slots: {
+          default: slotContent,
+        },
+      })
+      expect(
+        wrapper
+          .findAll('p')
+          .at(0)
+          .isVisible()
+      ).toBe(true)
+    })
+
+    it('subtitle', () => {
+      const slotContent = '<div>Test</div>'
+      const wrapper = shallowMount(Fieldset, {
+        propsData: {
+          title: 'title',
+          subtitle: 'subtitle',
+        },
+        slots: {
+          default: slotContent,
+        },
+      })
+      expect(
+        wrapper
+          .findAll('p')
+          .at(1)
+          .isVisible()
+      ).toBe(true)
+    })
+
+    it('helper', () => {
+      const slotContent = '<div>Test</div>'
+      const wrapper = shallowMount(Fieldset, {
+        propsData: {
+          title: 'title',
+          subtitle: 'subtitle',
+
+          helper: 'helper',
+        },
+        slots: {
+          default: slotContent,
+        },
+      })
+      expect(
+        wrapper
+          .findAll('p')
+          .at(2)
+          .isVisible()
+      ).toBe(true)
+    })
+  })
+})
