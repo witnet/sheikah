c493aafd49c12e743a8b9a71e91430a9ab2d8108
tommytrg <tomastrg22@gmail.com>
2020-04-13 17:47:00 +0200

    feat: implement BalanceData component

---
+++ src/components/BalanceData.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="table">
+    <p class="label">Available</p>
+    <p class="amount">
+      {{ available }} <span class="unit">{{ currency }}</span>
+    </p>
+
+    <p class="label">Locked</p>
+    <p class="amount">
+      {{ locked }} <span class="unit">{{ currency }}</span>
+    </p>
+
+    <p class="label total">Total</p>
+    <p class="amount total">
+      {{ total }} <span class="unit">{{ currency }}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+/**
+ * Show available, locked and total amount balances.
+ */
+export default {
+  name: 'BalanceData',
+  props: {
+    /**
+     * Specify in which unit display wits
+     * @values wit, microWit, nanoWit
+     */
+    currency: {
+      type: String,
+      requried: true,
+    },
+    /**
+     * Available wits the user owns
+     */
+    available: {
+      type: String,
+      required: true,
+    },
+    /**
+     * Locked wits that can't be spent yet
+     */
+    locked: {
+      type: String,
+      required: true,
+    },
+    /**
+     * Total amount of wits: available + locked
+     */
+    total: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/_colors.scss';
+
+.table {
+  width: 100%;
+  padding: 25px;
+  background-color: $purple-5;
+  border-radius: 4px;
+  box-shadow: 0 4px 4px rgb(155, 181, 224, 0.1);
+
+  display: grid;
+  grid-template: auto auto auto / auto 1fr;
+  row-gap: 15px;
+  margin-bottom: 10px;
+  .label {
+    font-size: 14px;
+    color: $white;
+
+    &.total {
+      font-size: 16px;
+      font-weight: bold;
+    }
+  }
+
+  .amount {
+    color: $white;
+    justify-self: end;
+    width: max-content;
+    font-size: 14px;
+    .unit {
+      font-size: 11px;
+      font-weight: normal;
+    }
+
+    &.total {
+      font-size: 16px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
+
+<docs>
+### Example
+
+```jsx
+  <BalanceData
+    locked="10"
+    available="40"
+    total="50"
+    currency="nanoWit"
+
+    :style="{ width: '300px' }"
+  />
+```
+
+</docs>
---
+++ tests/unit/src/components/BalanceData.spec.js
@@ -0,0 +1,75 @@
+import BalanceData from '@/components/BalanceData'
+
+describe('Balance.vue', () => {
+  it('should render the available amount', () => {
+    const wrapper = shallowMount(BalanceData, {
+      propsData: {
+        available: '1',
+        locked: '10',
+        total: '100',
+        currency: 'nanoWits',
+      },
+    })
+
+    expect(
+      wrapper
+        .findAll('p')
+        .at(0)
+        .text()
+    ).toBe('Available')
+    expect(
+      wrapper
+        .findAll('p')
+        .at(1)
+        .text()
+    ).toBe('1 nanoWits')
+  })
+
+  it('should render the locked amount', () => {
+    const wrapper = shallowMount(BalanceData, {
+      propsData: {
+        available: '1',
+        locked: '10',
+        total: '100',
+        currency: 'nanoWits',
+      },
+    })
+
+    expect(
+      wrapper
+        .findAll('p')
+        .at(2)
+        .text()
+    ).toBe('Locked')
+    expect(
+      wrapper
+        .findAll('p')
+        .at(3)
+        .text()
+    ).toBe('10 nanoWits')
+  })
+
+  it('should render the total amount', () => {
+    const wrapper = shallowMount(BalanceData, {
+      propsData: {
+        available: '1',
+        locked: '10',
+        total: '100',
+        currency: 'nanoWits',
+      },
+    })
+
+    expect(
+      wrapper
+        .findAll('p')
+        .at(4)
+        .text()
+    ).toBe('Total')
+    expect(
+      wrapper
+        .findAll('p')
+        .at(5)
+        .text()
+    ).toBe('100 nanoWits')
+  })
+})
