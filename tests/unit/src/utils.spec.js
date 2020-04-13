7ccee4c5cacd66076da08ad6d44e522523975833
tommytrg <tomastrg22@gmail.com>
2020-04-13 17:55:44 +0200

    style: run linter

--- tests/unit/src/utils.spec.js
+++ tests/unit/src/utils.spec.js
@@ -1,5 +1,6 @@
 import { areSoftEqualArrays, standardizeWitUnits } from '@/utils'
 import { WIT_UNIT } from '../../../src/constants'
+
 describe('areSoftEqualArrays', () => {
   it('check if two sorted arrays contains the same items', () => {
     const arr1 = [1, 2, 3, 4, 5]
@@ -31,6 +32,7 @@ describe('areSoftEqualArrays', () => {
     expect(areSoftEqualArrays(arr1, arr2)).toBe(false)
   })
 })
+
 describe('standardizeWitUnits', () => {
   describe('return the value in selected unit', () => {
     describe('wit', () => {
@@ -39,24 +41,28 @@ describe('standardizeWitUnits', () => {
         const result = standardizeWitUnits(4, WIT_UNIT.WIT)
         expect(result).toBe(expected)
       })
+
       it("trim 0's", () => {
         const expected = '0.0000004'
         const result = standardizeWitUnits(400, WIT_UNIT.WIT)
         expect(result).toBe(expected)
       })
     })
+
     describe('microWit', () => {
       it("without trim 0's", () => {
         const expected = '0.004'
         const result = standardizeWitUnits(4, WIT_UNIT.MICRO)
         expect(result).toBe(expected)
       })
+
       it("trim 0's", () => {
         const expected = '0.4'
         const result = standardizeWitUnits(400, WIT_UNIT.MICRO)
         expect(result).toBe(expected)
       })
     })
+
     it('nanoWit', () => {
       const expected = '40'
       const result = standardizeWitUnits(40, WIT_UNIT.NANO)
