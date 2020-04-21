042341bd5667670c3fe8dbbeb2d34569b9bfb44b
tommytrg <tomastrg22@gmail.com>
2020-04-21 17:28:25 +0200

    fix(styleguide): load styles in correct order

--- styleguide.config.js
+++ styleguide.config.js
@@ -11,11 +11,11 @@ module.exports = {
     'src/components/LayoutTransactions.vue',
   ],
   require: [
-    path.join(__dirname, './src/styles/app.global.scss'),
+    path.join(__dirname, './src/styles/reset.scss'),
     path.join(__dirname, './src/styles/_colors.scss'),
     path.join(__dirname, './src/styles/fonts.scss'),
-    path.join(__dirname, './src/styles/reset.scss'),
     path.join(__dirname, './src/styles/theme.scss'),
+    path.join(__dirname, './src/styles/app.global.scss'),
     path.join(__dirname, './styleguide/scripts.js'),
     path.join(__dirname, './styleguide/routerMock.js'),
   ],
