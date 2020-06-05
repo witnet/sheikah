d5a8b81597a507e32e2ad13dd3b75170400efdc3
tommytrg <tomastrg22@gmail.com>
2020-06-05 13:26:12 +0200

    fix(transaction-list): show pagination only when is required

---
+++ tests/unit/src/components/TransactionList.spec.js
@@ -0,0 +1,86 @@
+import TransactionList from '@/components/TransactionList.vue'
+import Transaction from '@/components/Transaction.vue'
+// eslint-disable-next-line import/no-relative-parent-imports
+import transactionsMock from '../../mocks/transactions'
+
+describe('Renders the correct elements when click is not triggered', () => {
+  it('render the number of transactions', () => {
+    const wrapper = shallowMount(TransactionList, {
+      propsData: {
+        transactions: transactionsMock,
+        currency: 'nanoWit',
+      },
+    })
+
+    expect(wrapper.find('[data-test="transactions-length"]').text()).toBe(
+      '36 transactions',
+    )
+  })
+
+  it('render empty state without transactions as prop', () => {
+    const wrapper = shallowMount(TransactionList, {
+      propsData: {
+        transactions: [],
+        currency: 'nanoWit',
+      },
+    })
+
+    expect(wrapper.find('[data-test="empty-transactions"]').isVisible()).toBe(
+      true,
+    )
+  })
+
+  it('should render less than 15 address', () => {
+    const wrapper = shallowMount(TransactionList, {
+      propsData: {
+        transactions: [
+          transactionsMock[0],
+          transactionsMock[1],
+          transactionsMock[2],
+        ],
+        currency: 'nanoWit',
+      },
+    })
+
+    expect(wrapper.findAll(Transaction).length).toBe(3)
+  })
+
+  it('should not render the pagination with less than 15 address', () => {
+    const wrapper = shallowMount(TransactionList, {
+      propsData: {
+        transactions: [
+          transactionsMock[0],
+          transactionsMock[1],
+          transactionsMock[2],
+        ],
+        currency: 'nanoWit',
+      },
+    })
+
+    expect(wrapper.find('[data-test="pagination"]').isVisible()).toBe(false)
+  })
+
+  describe('should render the pagination with more than 15 address', () => {
+    it('should render pagination', () => {
+      const wrapper = shallowMount(TransactionList, {
+        propsData: {
+          transactions: transactionsMock,
+          currency: 'nanoWit',
+        },
+      })
+
+      expect(wrapper.findAll(Transaction).length).toBe(15)
+    })
+
+    it('should render 15 addresses', () => {
+      const wrapper = shallowMount(TransactionList, {
+        propsData: {
+          transactions: transactionsMock,
+          currency: 'nanoWit',
+        },
+      })
+
+      expect(wrapper.find('[data-test="pagination"]').isVisible()).toBe(true)
+    })
+  })
+})
