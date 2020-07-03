326440b8d3e11595a6bdbf5193744f7274dd72be
tommytrg <tomastrg22@gmail.com>
2020-07-03 16:24:57 +0200

    wip

--- tests/e2e/createWallet.spec.js
+++ tests/e2e/createWallet.spec.js
@@ -1,8 +1,7 @@
 describe('Create wallet', () => {
   it('should create a wallet', async () => {
-    // const button = await app.client.getSelectedText('#btn')
-    // console.log('app.client', Object.keys(app.client))
-    // console.log('button', button)
+    const a = await app.client.waitUntilWindowLoaded().$('body').getHTML()
+    console.log('app.client', a)
     expect(true).toBe(true)
   })
 })
