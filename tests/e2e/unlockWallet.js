f52a72f46ee872778da9c1b97c0b92aa038879d4
tommytrg <tomastrg22@gmail.com>
2020-07-08 16:48:42 +0200

    wip

--- tests/e2e/unlockWallet.js
+++ tests/e2e/unlockWallet.js
@@ -12,33 +12,20 @@ describe('Create wallet', () => {
     await app.client
       .waitUntilWindowLoaded(10000)
       .click('')
-    const html = app.client.$('body').getHTML()
+    const html = await app.client.$('body').getHTML()
 
     console.log('html', html)
-    // await app.client.$('[data-test=new-seed-option]').click()
 
-    // await app.client.$('[data-test=next-step]').click()
+    await app.client.$('[data-test=passwordInput]').addValue('password')
 
-    // const seed = await app.client.$('[data-test=word-seed]').getText()
+    await sleep(1000)
 
-    // await app.client.$('[data-test=next-step]').click()
+    await app.client.$('[data-test=unlock-wallet]').click()
 
-    // await app.client.$('[data-test=seed-input]').addValue(seed)
+    await sleep(1000)
 
-    // await app.client.$('[data-test=next-step]').click()
+    await app.client.$('[data-test=main]')
 
-    // await app.client.$('[data-test=password]').addValue('password')
-    // await sleep(1000)
-
-    // await app.client.$('[data-test=repeat-password]').addValue('password')
-
-    // await sleep(1000)
-
-    // await app.client.$('[data-test=next-step]').click()
-
-    // await sleep(2000)
-
-    // await app.client.$('[data-test=main]')
     expect(true).toBe(true)
   })
 })
