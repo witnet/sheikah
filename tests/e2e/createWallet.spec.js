4d0363e3d9fc3fd7007f94135665bb7df2d3448a
tommytrg <tomastrg22@gmail.com>
2020-07-08 16:31:18 +0200

    wip

--- tests/e2e/createWallet.spec.js
+++ tests/e2e/createWallet.spec.js
@@ -4,34 +4,33 @@ describe('Create wallet', () => {
       .waitUntilWindowLoaded(10000)
       // .$('[data-test=create-wallet]')
       // .click('')
-    const html = await app.client.$('body').getHTML()
 
-    console.log('html', html)
+      await app.client.$('[data-test=new-seed-option]').click()
+      const html = await app.client.$('body').getHTML()
+      console.log('html', html)
 
-    // await app.client.$('[data-test=new-seed-option]').click()
+    await app.client.$('[data-test=next-step]').click()
 
-    // await app.client.$('[data-test=next-step]').click()
+    const seed = await app.client.$('[data-test=word-seed]').getText()
 
-    // const seed = await app.client.$('[data-test=word-seed]').getText()
+    await app.client.$('[data-test=next-step]').click()
 
-    // await app.client.$('[data-test=next-step]').click()
+    await app.client.$('[data-test=seed-input]').addValue(seed)
 
-    // await app.client.$('[data-test=seed-input]').addValue(seed)
+    await app.client.$('[data-test=next-step]').click()
 
-    // await app.client.$('[data-test=next-step]').click()
+    await app.client.$('[data-test=password]').addValue('password')
+    await sleep(1000)
 
-    // await app.client.$('[data-test=password]').addValue('password')
-    // await sleep(1000)
+    await app.client.$('[data-test=repeat-password]').addValue('password')
 
-    // await app.client.$('[data-test=repeat-password]').addValue('password')
+    await sleep(1000)
 
-    // await sleep(1000)
+    await app.client.$('[data-test=next-step]').click()
 
-    // await app.client.$('[data-test=next-step]').click()
+    await sleep(2000)
 
-    // await sleep(2000)
-
-    // await app.client.$('[data-test=main]')
+    await app.client.$('[data-test=main]')
     expect(true).toBe(true)
   })
 })
