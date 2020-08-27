166293b7a526b514407d70b45c0678cc51fc2d31
tommytrg <tomastrg22@gmail.com>
2020-08-27 17:45:45 +0200

    fix: set linux category in vue config instead of electron builder

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,11 @@ process.env.VUE_APP_VERSION = require('./package.json').version
 module.exports = {
   pluginOptions: {
     electronBuilder: {
+      builderOptions: {
+        linux: {
+          category: 'Network',
+        },
+      },
       nodeIntegration: true,
     },
   },
