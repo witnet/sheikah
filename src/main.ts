bd187e5e7c652431ba274fc8b04907dc441d25e8
tommy <tomastrg22@gmail.com>
2022-11-02 17:35:53 +0100

    make fontawesome work

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import i18n from './plugins/i18n'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // import './plugins/element.js'
-// import './fontAwesome'
+import './fontAwesome'
 // import '@/directives'
 // import '@/ipcHandlers'
 
@@ -33,7 +33,7 @@ import 'element-plus/dist/index.css'
 //   window.vm = vm
 // }
 
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { createApp } from 'vue'
 import App from './App.vue'
 
@@ -43,6 +43,7 @@ const app = createApp(App)
   app.use(router)
   app.use(store)
   app.use(i18n)
+  app.component('font-awesome-icon', FontAwesomeIcon)
 
   app.mount('#app')
   .$nextTick(() => {
