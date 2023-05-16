61a4de03916360a9413ff51f00c67ff58163cdc1
gabaldon <maria@otherplane.com>
2023-05-16 16:24:46 +0200

    fix: avoid downloading not compatible wallet version

--- src/background/walletManager.ts
+++ src/background/walletManager.ts
@@ -57,8 +57,8 @@ export class WalletManager {
 
   //  Start running the wallet release and download it when is necessary
   public async run() {
-    this.latestWitnetRustVersion = await getLatestWitnetRustRelease()
     if (this.existDirectory) {
+      this.latestWitnetRustVersion = await getLatestWitnetRustRelease()
       // Check if latest version is compatible or needs to be downloaded
       try {
         const versionName = fs.readFileSync(
