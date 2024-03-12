d299e271c6a9162d0dc5d93cdc5964e1ddeae109
gabaldon <maria@otherplane.com>
2024-03-12 11:45:22 +0100

    fix: use localStorageWrapper in language switcher

--- src/components/SettingsLanguage.vue
+++ src/components/SettingsLanguage.vue
@@ -2,7 +2,7 @@
   <Card class="card" :title="$t('language')" :border="false" shadow="thin">
     <p class="text">{{ $t('select_language') }}</p>
     <Select
-      v-model="actualLanguage"
+      v-model="currentLanguage"
       data-test="select-unit"
       type="big"
       :options="options"
@@ -16,11 +16,11 @@ import Card from '@/components/card/Card.vue'
 import Select from '@/components/Select.vue'
 import { LANGUAGES } from '@/constants'
 import { useI18n } from 'vue-i18n'
-import { computed, toRefs } from 'vue'
+import { computed } from 'vue'
+import localStorageWrapper from '@/services/localStorageWrapper'
 
 const { locale } = useI18n()
 const store = useStore()
-const { localStorage } = toRefs(store.state.wallet)
 
 const options = computed(() =>
   Object.values(LANGUAGES).map(language => ({
@@ -29,15 +29,15 @@ const options = computed(() =>
   })),
 )
 
-const actualLanguage = computed({
+const currentLanguage = computed({
   get: () => ({
     primaryText: LANGUAGES[locale.value].name,
     value: LANGUAGES[locale.value].locale,
   }),
   set: val => {
     locale.value = val.value
-    localStorage.value.setLanguageSettings(val.value)
-    store.commit('updateDRLanguage', val)
+    localStorageWrapper.setLanguageSettings(val.value)
+    store.commit('updateDRLanguage', val.value)
   },
 })
 </script>
