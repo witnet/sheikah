1a80a46c891d859331c7c9f512bb2d199e1a5bca
gabaldon <maria@otherplane.com>
2024-04-02 11:08:34 +0200

    fix: update electron builder config

--- scripts/notarize.cjs
+++ scripts/notarize.cjs
@@ -1,29 +1,18 @@
 require('dotenv').config()
-import { notarize } from '@electron/notarize'
+const { notarize } = require('@electron/notarize')
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context
-
-  // skip notarization for no macOS platforms
   if (electronPlatformName !== 'darwin') {
     return
   }
 
-  // Skip notarization when SIGN env is false
-  if (
-    !process.env.SIGN ||
-    process.env.SIGN === 'false' ||
-    process.env.SIGN === '0'
-  ) {
-    return
-  }
-
   const appName = context.packager.appInfo.productFilename
-
   return await notarize({
-    appBundleId: 'io.witnet.sheikah',
+    tool: 'notarytool',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
+    teamId: process.env.APPLETEAMID,
   })
 }
