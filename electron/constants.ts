4137b7a17631958816010350078c34e06e7c14d0
gabaldon <maria@otherplane.com>
2024-04-23 15:56:20 +0200

    feat: set latest compatible wallet version to 1.7.1

--- electron/constants.ts
+++ electron/constants.ts
@@ -67,7 +67,7 @@ export const STATUS_PATH = {
 
 export const WITNET_CONFIG_FILE_NAME = 'witnet.toml'
 
-export const WITNET_RUST_VERSION = '1.5.6'
+export const WITNET_RUST_VERSION = '1.7.1'
 
 export const DEFAULT_WALLET_LOG_LEVEL = 'error'
 
