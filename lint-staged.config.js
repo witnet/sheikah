a34f00f476e52a2fa8337713316088c83c0cd858
tommytrg <tomastrg22@gmail.com>
2024-07-25 12:17:05 +0200

    chore: remove unused files

--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  '*.js': ['pnpm lint:eslint', 'pnpm lint:prettier'],
-  '{!(package)*.json,.!(browserslist)*rc}': [
-    'pnpm lint:prettier --parser json',
-  ],
-  'package.json': ['pnpm lint:prettier'],
-  '*.vue': ['pnpm lint:eslint', 'pnpm lint:stylelint', 'pnpm lint:prettier'],
-  '*.scss': ['pnpm lint:stylelint', 'pnpm lint:prettier'],
-}
